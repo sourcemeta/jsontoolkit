7e29cf02300a3a928f6608652a6775e886b52698
Juan Cruz Viotti <jv@jviotti.com>
2022-12-07 21:37:13 -0400

    Basic GTest setup
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/json_test.cc
@@ -0,0 +1,9 @@
+#include <gtest/gtest.h>
+#include <jsontoolkit/json.h>
+
+TEST(JSON, XXXXXXX) { EXPECT_TRUE(true); }
+
+int main(int argc, char **argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
