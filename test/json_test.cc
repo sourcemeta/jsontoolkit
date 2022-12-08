b5dd4b7de18247fbda1bfa8be0ad631e04da339f
Juan Cruz Viotti <jv@jviotti.com>
2022-12-07 21:39:13 -0400

    Link against GTest_main
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/json_test.cc
+++ test/json_test.cc
@@ -2,8 +2,3 @@
 #include <jsontoolkit/json.h>
 
 TEST(JSON, XXXXXXX) { EXPECT_TRUE(true); }
-
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
-}
