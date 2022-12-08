678ea60445062eff8e97ee61bcd64323b6cdf369
Juan Cruz Viotti <jv@jviotti.com>
2022-12-07 21:47:06 -0400

    Basic rapidjson structure
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- backends/rapidjson/json.cc
+++ backends/rapidjson/json.cc
@@ -1,3 +1,19 @@
 #include <jsontoolkit/json.h>
 
-sourcemeta::jsontoolkit::JSON::JSON(const std::string &) {}
+sourcemeta::jsontoolkit::JSON::JSON(const std::string &json)
+    : is_top_level{true}, is_owned{true} {
+  rapidjson::Document *document = new rapidjson::Document();
+  document->Parse(json);
+  this->data = document;
+  this->allocator = &document->GetAllocator();
+}
+
+sourcemeta::jsontoolkit::JSON::~JSON() {
+  if (!is_owned) {
+    return;
+  } else if (is_top_level) {
+    static_cast<rapidjson::Document *>(this->data)->~GenericDocument();
+  } else {
+    this->data->~GenericValue();
+  }
+}
--- include/jsontoolkit/json.h
+++ include/jsontoolkit/json.h
@@ -4,13 +4,26 @@
 #include <string>
 
 #if defined(JSONTOOLKIT_BACKEND_RAPIDJSON)
-#include <rapidjson/document.h> // rapidjson::Document
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#pragma clang diagnostic ignored "-Wambiguous-reversed-operator"
+#include <rapidjson/document.h> // rapidjson::Value, rapidjson::Document
+#pragma clang diagnostic pop
 #endif
 
 namespace sourcemeta::jsontoolkit {
 class JSON {
 public:
   JSON(const std::string &json);
+  ~JSON();
+
+private:
+#if defined(JSONTOOLKIT_BACKEND_RAPIDJSON)
+  rapidjson::Value *data;
+  rapidjson::Document::AllocatorType *allocator;
+  const bool is_top_level = false;
+  const bool is_owned = true;
+#endif
 };
 } // namespace sourcemeta::jsontoolkit
 
