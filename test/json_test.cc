f9f10cdfbf8caba529717650281b1802c217316d
Juan Cruz Viotti <jv@jviotti.com>
2022-12-08 21:46:57 -0400

    Add .at() tests
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/json_test.cc
+++ test/json_test.cc
@@ -25,3 +25,19 @@ TEST(JSON, is_object_empty_object) {
   const sourcemeta::jsontoolkit::JSON document{"{}"};
   EXPECT_TRUE(document.is_object());
 }
+
+TEST(JSON, is_object_at_boolean_1) {
+  const sourcemeta::jsontoolkit::JSON document{"{ \"foo\": false }"};
+  EXPECT_TRUE(document.at("foo").is_boolean());
+}
+
+TEST(JSON, is_object_at_boolean_2) {
+  const sourcemeta::jsontoolkit::JSON document{"{ \"foo\": 1 }"};
+  EXPECT_FALSE(document.at("foo").is_boolean());
+}
+
+TEST(JSON, is_object_at_nested_boolean) {
+  const sourcemeta::jsontoolkit::JSON document{
+      "{ \"foo\": { \"bar\": { \"baz\": true } } }"};
+  EXPECT_TRUE(document.at("foo").at("bar").at("baz").is_boolean());
+}
