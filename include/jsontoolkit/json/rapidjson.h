9fd87389a591cf6bd55932d2140b3b52b6537b21
Juan Cruz Viotti <jv@jviotti.com>
2022-12-23 15:42:38 -0400

    Fix includes
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- include/jsontoolkit/json/rapidjson.h
+++ include/jsontoolkit/json/rapidjson.h
@@ -11,10 +11,10 @@
 #pragma clang diagnostic push
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #pragma clang diagnostic ignored "-Wambiguous-reversed-operator"
-#include "rapidjson/prettywriter.h"   // rapidjson::PrettyWriter
 #include <rapidjson/document.h>       // rapidjson::Value, rapidjson::Document
 #include <rapidjson/error/en.h>       // rapidjson::GetParseError_En
 #include <rapidjson/ostreamwrapper.h> // rapidjson::OStreamWrapper
+#include <rapidjson/prettywriter.h>   // rapidjson::PrettyWriter
 #include <rapidjson/writer.h>         // rapidjson::Writer
 #pragma clang diagnostic pop
 
