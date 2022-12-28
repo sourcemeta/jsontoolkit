4896aa10efbc2304c8dbcac50e96c4112d9e9707
Juan Cruz Viotti <jv@jviotti.com>
2022-12-27 20:32:56 -0400

    Fix pre-processor
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- include/jsontoolkit/read.h
+++ include/jsontoolkit/read.h
@@ -1,6 +1,10 @@
 #ifndef JSONTOOLKIT_READ_H_
 #define JSONTOOLKIT_READ_H_
 
+#if JSONTOOLKIT_BACKEND == rapidjson
 #include "json/rapidjson/read.h"
+#else
+#error Unknown JSON Toolkit backend
+#endif
 
 #endif
--- include/jsontoolkit/write.h
+++ include/jsontoolkit/write.h
@@ -1,6 +1,10 @@
 #ifndef JSONTOOLKIT_WRITE_H_
 #define JSONTOOLKIT_WRITE_H_
 
+#if JSONTOOLKIT_BACKEND == rapidjson
 #include "json/rapidjson/write.h"
+#else
+#error Unknown JSON Toolkit backend
+#endif
 
 #endif
