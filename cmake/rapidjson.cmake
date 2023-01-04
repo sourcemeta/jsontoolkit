4cfd37a4f04d42b26b71a6f9b686d17fd58a9c5c
Juan Cruz Viotti <jv@jviotti.com>
2023-01-04 19:24:02 -0400

    Support a custom backend path (#29)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/rapidjson.cmake
+++ cmake/rapidjson.cmake
@@ -1,6 +1,11 @@
-# RapidJSON's find package implementation populates RAPIDJSON_INCLUDE_DIRS
-# See https://github.com/rjeczalik/rapidjson/blob/master/cmake/Findrapidjson.cmake
-find_package(RapidJSON REQUIRED)
+if(JSONTOOLKIT_BACKEND_PATH STREQUAL "")
+  # RapidJSON's find package implementation populates RAPIDJSON_INCLUDE_DIRS
+  # See https://github.com/rjeczalik/rapidjson/blob/master/cmake/Findrapidjson.cmake
+  find_package(RapidJSON REQUIRED)
+else()
+  set(RAPIDJSON_INCLUDE_DIRS "${JSONTOOLKIT_BACKEND_PATH}/include")
+endif()
+
 add_library(rapidjson INTERFACE)
 target_include_directories(rapidjson INTERFACE "${RAPIDJSON_INCLUDE_DIRS}")
 # Build RapidJSON using std::string
