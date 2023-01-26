85bca0b453998dae9291fd6dadefce7ad0a9be3a
Juan Cruz Viotti <jv@jviotti.com>
2023-01-26 14:36:34 -0400

    Implement a default JSON Schema 2020-12 schema walker (#53)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ include/jsontoolkit/jsonschema/default_walker.h
@@ -0,0 +1,15 @@
+#ifndef JSONTOOLKIT_JSONSCHEMA_DEFAULT_WALKER_H_
+#define JSONTOOLKIT_JSONSCHEMA_DEFAULT_WALKER_H_
+
+#include <jsontoolkit/jsonschema/walker.h>
+
+namespace sourcemeta::jsontoolkit {
+
+auto default_schema_walker(
+    const std::string &keyword,
+    const std::unordered_map<std::string, bool> &vocabularies)
+    -> sourcemeta::jsontoolkit::schema_walker_strategy_t;
+
+} // namespace sourcemeta::jsontoolkit
+
+#endif
