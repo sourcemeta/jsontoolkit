19f26607574fbbbe772ea87c65df8f946ae1bbf3
Juan Cruz Viotti <jv@jviotti.com>
2023-02-04 10:58:11 -0400

    Implement a flat subschema iterator (#57)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- include/jsontoolkit/jsonschema/walker.h
+++ include/jsontoolkit/jsonschema/walker.h
@@ -1,14 +1,9 @@
 #ifndef JSONTOOLKIT_JSONSCHEMA_WALKER_H_
 #define JSONTOOLKIT_JSONSCHEMA_WALKER_H_
 
-#include <jsontoolkit/json.h>
-#include <jsontoolkit/jsonschema/resolver.h>
-
 #include <functional>    // std::function
-#include <optional>      // std::optional
 #include <string>        // std::string
 #include <unordered_map> // std::unordered_map
-#include <vector>        // std::vector
 
 namespace sourcemeta::jsontoolkit {
 
@@ -25,31 +20,7 @@ enum class schema_walker_strategy_t {
 using schema_walker_t = std::function<schema_walker_strategy_t(
     const std::string &, const std::unordered_map<std::string, bool> &)>;
 
-class SchemaWalker {
-public:
-  SchemaWalker(const Value &input, const schema_walker_t &walker,
-               const schema_resolver_t &resolver,
-               const std::optional<std::string> &default_metaschema);
-  inline auto begin() const -> decltype(auto) {
-    return this->subschemas.cbegin();
-  }
-  inline auto end() const -> decltype(auto) { return this->subschemas.cend(); }
-
-private:
-  auto walk(const Value &subschema, const schema_walker_t &walker,
-            const schema_resolver_t &resolver, const std::string &metaschema)
-      -> void;
-  auto walk_array(const Value &array, const schema_walker_t &walker,
-                  const schema_resolver_t &resolver,
-                  const std::string &metaschema) -> void;
-  auto walk_object(const Value &object, const schema_walker_t &walker,
-                   const schema_resolver_t &resolver,
-                   const std::string &metaschema) -> void;
-  auto walk_schema(const Value &schema, const schema_walker_t &walker,
-                   const schema_resolver_t &resolver,
-                   const std::string &metaschema) -> void;
-  std::vector<std::reference_wrapper<const Value>> subschemas;
-};
+enum class schema_walker_type_t { Deep, Flat };
 
 } // namespace sourcemeta::jsontoolkit
 
