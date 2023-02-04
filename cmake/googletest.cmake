c90be774cbdc81c24290d15b2c4d3dcc9b0b33d2
Juan Cruz Viotti <jv@jviotti.com>
2023-02-04 11:18:08 -0400

    More fixes
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/googletest.cmake
+++ cmake/googletest.cmake
@@ -9,3 +9,11 @@ if(NOT GTest_FOUND)
     message(FATAL_ERROR "Could NOT find GTest")
   endif()
 endif()
+
+# Some GNU/Linux distros lack the namespace
+if(NOT TARGET GTest::gtest)
+  add_library(GTest::gtest ALIAS gtest)
+endif()
+if(NOT TARGET GTest::gtest_main)
+  add_library(GTest::gtest_main ALIAS gtest_main)
+endif()
