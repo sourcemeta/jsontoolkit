8ee922cc0130ece4eb9aacf73de6a2bd94d16b53
Juan Cruz Viotti <jv@jviotti.com>
2023-04-14 14:10:06 -0400

    Make all tests pass
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/contrib/is_same_stdin.bat
+++ test/contrib/is_same_stdin.bat
@@ -4,10 +4,18 @@ setlocal
 set "PROGRAM=%1"
 set "EXPECTED=%2"
 set "ARGUMENT=%3"
+set "SUBARGUMENT=%4"
 
 set "TEMP_OUTPUT=test-output-%RANDOM%-%RANDOM%.txt"
-echo Running %PROGRAM% ^< %ARGUMENT%  1>&2
-"%PROGRAM%" < "%ARGUMENT%" >"%TEMP_OUTPUT%" || exit /b 1
+
+if "%SUBARGUMENT%"=="" (
+  echo Running %PROGRAM% ^< %ARGUMENT%  1>&2
+  "%PROGRAM%" < "%ARGUMENT%" >"%TEMP_OUTPUT%" || exit /b 1
+) else (
+  echo Running %PROGRAM% %SUBARGUMENT% ^< %ARGUMENT%  1>&2
+  "%PROGRAM%" "%SUBARGUMENT%" < "%ARGUMENT%" >"%TEMP_OUTPUT%" || exit /b 1
+)
+
 fc "%TEMP_OUTPUT%" "%EXPECTED%" > nul
 if errorlevel 1 (
   echo Got 1>&2
