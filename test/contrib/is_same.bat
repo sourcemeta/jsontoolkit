71bdc0c466a2a26d4a9830fd1a088db0656d0a4f
Juan Cruz Viotti <jv@jviotti.com>
2023-04-14 13:51:49 -0400

    Revise is_same.bat
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/contrib/is_same.bat
+++ test/contrib/is_same.bat
@@ -1,14 +1,21 @@
 @echo off
-setlocal enabledelayedexpansion
+setlocal
+
 set "PROGRAM=%1"
 set "EXPECTED=%2"
 set "ARGUMENT=%3"
-echo Running %PROGRAM% %ARGUMENT%
-set "OUTPUT="
-for /F "delims=" %%g in ('%PROGRAM% %ARGUMENT%') do (set "OUTPUT=!OUTPUT!%%g")
-set /P EXPECTED_OUTPUT=<%EXPECTED%
-if not "%OUTPUT%"=="%EXPECTED_OUTPUT%" (
-  echo Got: %OUTPUT%
-  echo Expected: %EXPECTED_OUTPUT%
+
+set "TEMP_OUTPUT=test-output-%RANDOM%-%RANDOM%.txt"
+echo Running %PROGRAM% %ARGUMENT% 1>&2
+"%PROGRAM%" "%ARGUMENT%" >"%TEMP_OUTPUT%"
+fc "%TEMP_OUTPUT%" "%EXPECTED%" > nul
+if errorlevel 1 (
+  echo Got 1>&2
+  type "%TEMP_OUTPUT%"
+  del "%TEMP_OUTPUT%"
+  echo Expected 1>&2
+  type "%EXPECTED%"
   exit /b 1
+) else (
+  del "%TEMP_OUTPUT%"
 )
