e63e3ebc5f02bb9c16265a11f84946359b17bf1e
Juan Cruz Viotti <jv@jviotti.com>
2023-07-05 21:15:23 -0400

    Make the project consumable by CMake `find_package()`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/packaging/find_package/hello.cc
@@ -0,0 +1,12 @@
+#include <jsontoolkit/json.h>
+
+#include <cstdlib>  // EXIT_SUCCESS
+#include <iostream> // std::cout
+
+auto main() -> int {
+  const sourcemeta::jsontoolkit::JSON document{
+      sourcemeta::jsontoolkit::from("Hello World")};
+  sourcemeta::jsontoolkit::stringify(document, std::cout);
+  std::cout << std::endl;
+  return EXIT_SUCCESS;
+}
