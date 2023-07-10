5eb95ecb1b8cc85abba785ef3ecc71b86cc0709a
Juan Cruz Viotti <jv@jviotti.com>
2023-07-10 17:44:13 -0400

    Install library CMake exports
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/rapidjson.cmake
+++ cmake/rapidjson.cmake
@@ -10,7 +10,15 @@ endif()
 if(NOT TARGET rapidjson)
   add_library(rapidjson INTERFACE)
   target_include_directories(rapidjson INTERFACE "${RAPIDJSON_INCLUDE_DIRS}")
+  # We need RapidJSON to be part of an export set in order to export
+  # targets that depend on it. That said, we never install this component.
+  install(TARGETS rapidjson EXPORT rapidjson COMPONENT rapidjson)
+  install(EXPORT rapidjson
+    DESTINATION "${CMAKE_INSTALL_LIBDIR}/cmake/rapidjson"
+    COMPONENT rapidjson
+    EXCLUDE_FROM_ALL)
 endif()
 
 # Build RapidJSON using std::string
 target_compile_definitions(rapidjson INTERFACE RAPIDJSON_HAS_STDSTRING)
+
