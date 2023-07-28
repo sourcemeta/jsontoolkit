c7539985a58fc1f4682f2f1693be329ad4b7da86
Juan Cruz Viotti <jv@jviotti.com>
2023-07-28 13:39:00 -0400

    Upgrade Noa to handle RPATHs
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- vendor/noa/cmake/noa/defaults.cmake
+++ vendor/noa/cmake/noa/defaults.cmake
@@ -1,4 +1,6 @@
 # Hide symbols from shared libraries by default
+# In certain compilers, like GCC and Clang,
+# symbols are visible by default.
 set(CMAKE_VISIBILITY_INLINES_HIDDEN YES)
 if("CXX" IN_LIST NOA_LANGUAGES)
   set(CMAKE_CXX_VISIBILITY_PRESET hidden)
@@ -13,3 +15,10 @@ endif()
 # Export compile commands by default.
 # It is very useful for IDE integration, linting, etc
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
+# Prevent DT_RPATH/DT_RUNPATH problem
+# This problem is not present on Apple platforms.
+# See https://www.youtube.com/watch?v=m0DwB4OvDXk
+if(NOT APPLE)
+  set(CMAKE_INSTALL_RPATH $ORIGIN)
+endif()
