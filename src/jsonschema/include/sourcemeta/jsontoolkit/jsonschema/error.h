4b5877bbc4c2565c76f7a8ddfbc9ae59187b8e43
Juan Cruz Viotti <jv@jviotti.com>
2023-08-01 12:20:22 -0400

    Fix several shared library generation warnings on Windows (#186)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsonschema/include/sourcemeta/jsontoolkit/jsonschema/error.h
+++ src/jsonschema/include/sourcemeta/jsontoolkit/jsonschema/error.h
@@ -9,6 +9,13 @@
 
 namespace sourcemeta::jsontoolkit {
 
+// Exporting symbols that depends on the standard C++ library is considered
+// safe.
+// https://learn.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-2-c4275?view=msvc-170&redirectedfrom=MSDN
+#if defined(_MSC_VER)
+#pragma warning(disable : 4251 4275)
+#endif
+
 class SOURCEMETA_JSONTOOLKIT_JSONSCHEMA_EXPORT SchemaError
     : public std::exception {
 public:
@@ -38,6 +45,10 @@ private:
   std::string identifier_;
   std::string message_;
 };
+
+#if defined(_MSC_VER)
+#pragma warning(default : 4251 4275)
+#endif
 
 } // namespace sourcemeta::jsontoolkit
 
