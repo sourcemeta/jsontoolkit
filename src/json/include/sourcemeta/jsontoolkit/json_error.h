8d34e9d4f828b57d78a70c4821757a5a4b0c2bc5
Juan Cruz Viotti <jv@jviotti.com>
2023-08-16 20:37:29 -0400

    Allow `ParseError` to be exported on Windows (#195)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/json/include/sourcemeta/jsontoolkit/json_error.h
+++ src/json/include/sourcemeta/jsontoolkit/json_error.h
@@ -8,6 +8,13 @@
 
 namespace sourcemeta::jsontoolkit {
 
+// Exporting symbols that depends on the standard C++ library is considered
+// safe.
+// https://learn.microsoft.com/en-us/cpp/error-messages/compiler-warnings/compiler-warning-level-2-c4275?view=msvc-170&redirectedfrom=MSDN
+#if defined(_MSC_VER)
+#pragma warning(disable : 4251 4275)
+#endif
+
 /// @ingroup json
 /// This class represents a parsing error.
 class SOURCEMETA_JSONTOOLKIT_JSON_EXPORT ParseError : public std::exception {
@@ -32,6 +39,10 @@ private:
   std::uint64_t line_;
   std::uint64_t column_;
 };
+
+#if defined(_MSC_VER)
+#pragma warning(default : 4251 4275)
+#endif
 
 } // namespace sourcemeta::jsontoolkit
 
