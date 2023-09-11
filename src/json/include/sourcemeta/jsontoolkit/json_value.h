ed47394fa77fc58746f03dfbdc18589eb8fa7db3
Juan Cruz Viotti <jv@jviotti.com>
2023-09-11 15:25:47 -0400

    Properly support C++20 in Unikraft  (#234)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/json/include/sourcemeta/jsontoolkit/json_value.h
+++ src/json/include/sourcemeta/jsontoolkit/json_value.h
@@ -887,8 +887,7 @@ public:
   /// ```
   [[nodiscard]] auto defines(const String &key) const -> bool {
     assert(this->is_object());
-    return std::get<Object>(this->data).data.find(key) !=
-           std::get<Object>(this->data).data.end();
+    return std::get<Object>(this->data).data.contains(key);
   }
 
   /// This method checks whether an input JSON object defines at least one given
@@ -1190,7 +1189,7 @@ public:
 
     std::set<String, std::less<String>, Allocator<String>> blacklist;
     for (const auto &pair : this->as_object()) {
-      if (whitelist.find(pair.first) == whitelist.end()) {
+      if (!whitelist.contains(pair.first)) {
         blacklist.insert(pair.first);
       }
     }
