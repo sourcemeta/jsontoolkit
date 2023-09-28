66986ff2fec14bc6853455417750c17ed0f87714
Juan Cruz Viotti <jv@jviotti.com>
2023-09-28 15:06:41 -0400

    Make JSON Pointer grammar header private (#249)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsonpointer/grammar.h
+++ src/jsonpointer/grammar.h
@@ -2,7 +2,6 @@
 #define JSONTOOLKIT_JSONPOINTER_GRAMMAR_H_
 
 namespace sourcemeta::jsontoolkit::internal {
-template <typename CharT> static constexpr CharT token_pointer_hyphen{'\u002D'};
 template <typename CharT> static constexpr CharT token_pointer_slash{'\u002F'};
 template <typename CharT> static constexpr CharT token_pointer_tilde{'\u007E'};
 template <typename CharT> static constexpr CharT token_pointer_zero{'\u0030'};
@@ -22,6 +21,27 @@ template <typename CharT>
 static constexpr CharT token_pointer_escape_tab{'\u0074'};
 template <typename CharT>
 static constexpr CharT token_pointer_reverse_solidus{'\u005C'};
+
+template <typename CharT>
+static constexpr CharT token_pointer_number_zero{'\u0030'};
+template <typename CharT>
+static constexpr CharT token_pointer_number_one{'\u0031'};
+template <typename CharT>
+static constexpr CharT token_pointer_number_two{'\u0032'};
+template <typename CharT>
+static constexpr CharT token_pointer_number_three{'\u0033'};
+template <typename CharT>
+static constexpr CharT token_pointer_number_four{'\u0034'};
+template <typename CharT>
+static constexpr CharT token_pointer_number_five{'\u0035'};
+template <typename CharT>
+static constexpr CharT token_pointer_number_six{'\u0036'};
+template <typename CharT>
+static constexpr CharT token_pointer_number_seven{'\u0037'};
+template <typename CharT>
+static constexpr CharT token_pointer_number_eight{'\u0038'};
+template <typename CharT>
+static constexpr CharT token_pointer_number_nine{'\u0039'};
 } // namespace sourcemeta::jsontoolkit::internal
 
 #endif
