8f88d3e20bb2da929dadf06d15ae586d256d7bae
Juan Cruz Viotti <jv@jviotti.com>
2023-10-02 14:57:53 -0400

    Split base dialect tests by dialect (#269)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/jsonschema/jsonschema_base_dialect_draft0_test.cc
@@ -0,0 +1,61 @@
+#include <gtest/gtest.h>
+#include <sourcemeta/jsontoolkit/json.h>
+#include <sourcemeta/jsontoolkit/jsonschema.h>
+
+TEST(JSONSchema_base_dialect_draft0, jsonschema_draft_hyperschema) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-00/hyper-schema#",
+    "type": "object"
+  })JSON");
+  const std::optional<std::string> base_dialect{
+      sourcemeta::jsontoolkit::base_dialect(
+          document, sourcemeta::jsontoolkit::official_resolver)
+          .get()};
+  EXPECT_TRUE(base_dialect.has_value());
+  EXPECT_EQ(base_dialect.value(),
+            "http://json-schema.org/draft-00/hyper-schema#");
+}
+
+TEST(JSONSchema_base_dialect_draft0, jsonschema_draft_schema) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-00/schema#",
+    "type": "object"
+  })JSON");
+  const std::optional<std::string> base_dialect{
+      sourcemeta::jsontoolkit::base_dialect(
+          document, sourcemeta::jsontoolkit::official_resolver)
+          .get()};
+  EXPECT_TRUE(base_dialect.has_value());
+  EXPECT_EQ(base_dialect.value(),
+            "http://json-schema.org/draft-00/hyper-schema#");
+}
+
+TEST(JSONSchema_base_dialect_draft0, jsonschema_draft_jsonref) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-00/json-ref#"
+  })JSON");
+  const std::optional<std::string> base_dialect{
+      sourcemeta::jsontoolkit::base_dialect(
+          document, sourcemeta::jsontoolkit::official_resolver)
+          .get()};
+  EXPECT_TRUE(base_dialect.has_value());
+  EXPECT_EQ(base_dialect.value(),
+            "http://json-schema.org/draft-00/hyper-schema#");
+}
+
+TEST(JSONSchema_base_dialect_draft0, jsonschema_draft_links) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-00/links#"
+  })JSON");
+  const std::optional<std::string> base_dialect{
+      sourcemeta::jsontoolkit::base_dialect(
+          document, sourcemeta::jsontoolkit::official_resolver)
+          .get()};
+  EXPECT_TRUE(base_dialect.has_value());
+  EXPECT_EQ(base_dialect.value(),
+            "http://json-schema.org/draft-00/hyper-schema#");
+}
