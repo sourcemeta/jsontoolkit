dce04ba9de6336d9dee52030dfe1f3ecadcc34a9
Juan Cruz Viotti <jv@jviotti.com>
2023-10-09 19:56:32 -0400

    Properly test `id()` across dialects (#299)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

---
+++ test/jsonschema/jsonschema_id_draft3_test.cc
@@ -0,0 +1,105 @@
+#include <gtest/gtest.h>
+#include <sourcemeta/jsontoolkit/json.h>
+#include <sourcemeta/jsontoolkit/jsonschema.h>
+
+static auto test_resolver(std::string_view identifier)
+    -> std::future<std::optional<sourcemeta::jsontoolkit::JSON>> {
+  std::promise<std::optional<sourcemeta::jsontoolkit::JSON>> promise;
+
+  if (identifier == "https://sourcemeta.com/metaschema") {
+    promise.set_value(sourcemeta::jsontoolkit::parse(R"JSON({
+      "id": "https://sourcemeta.com/metaschema",
+      "$schema": "http://json-schema.org/draft-03/schema#"
+    })JSON"));
+  } else {
+    return sourcemeta::jsontoolkit::official_resolver(identifier);
+  }
+
+  return promise.get_future();
+}
+
+TEST(JSONSchema_id_draft3, valid_one_hop) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "id": "https://example.com/my-schema",
+    "$schema": "https://sourcemeta.com/metaschema"
+  })JSON");
+  std::optional<std::string> id{
+      sourcemeta::jsontoolkit::id(document, test_resolver).get()};
+  EXPECT_TRUE(id.has_value());
+  EXPECT_EQ(id.value(), "https://example.com/my-schema");
+}
+
+TEST(JSONSchema_id_draft3, new_one_hop) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$id": "https://example.com/my-schema",
+    "$schema": "https://sourcemeta.com/metaschema"
+  })JSON");
+  std::optional<std::string> id{
+      sourcemeta::jsontoolkit::id(document, test_resolver).get()};
+  EXPECT_FALSE(id.has_value());
+}
+
+TEST(JSONSchema_id_draft3, id_boolean_default_dialect) {
+  const sourcemeta::jsontoolkit::JSON document{true};
+  std::optional<std::string> id{
+      sourcemeta::jsontoolkit::id(document,
+                                  sourcemeta::jsontoolkit::official_resolver,
+                                  "http://json-schema.org/draft-03/schema#")
+          .get()};
+  EXPECT_FALSE(id.has_value());
+}
+
+TEST(JSONSchema_id_draft3, empty_object_default_dialect) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse("{}");
+  std::optional<std::string> id{
+      sourcemeta::jsontoolkit::id(document,
+                                  sourcemeta::jsontoolkit::official_resolver,
+                                  "http://json-schema.org/draft-03/schema#")
+          .get()};
+  EXPECT_FALSE(id.has_value());
+}
+
+TEST(JSONSchema_id_draft3, valid_id) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "id": "https://example.com/my-schema",
+    "$schema": "http://json-schema.org/draft-03/schema#"
+  })JSON");
+  std::optional<std::string> id{
+      sourcemeta::jsontoolkit::id(document,
+                                  sourcemeta::jsontoolkit::official_resolver)
+          .get()};
+  EXPECT_TRUE(id.has_value());
+  EXPECT_EQ(id.value(), "https://example.com/my-schema");
+}
+
+TEST(JSONSchema_id_draft3, new_id) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$id": "https://example.com/my-schema",
+    "$schema": "http://json-schema.org/draft-03/schema#"
+  })JSON");
+  std::optional<std::string> id{
+      sourcemeta::jsontoolkit::id(document,
+                                  sourcemeta::jsontoolkit::official_resolver)
+          .get()};
+  EXPECT_FALSE(id.has_value());
+}
+
+TEST(JSONSchema_id_draft3, default_dialect_precedence) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "id": "https://example.com/my-schema",
+    "$schema": "http://json-schema.org/draft-03/schema#"
+  })JSON");
+  std::optional<std::string> id{
+      sourcemeta::jsontoolkit::id(
+          document, sourcemeta::jsontoolkit::official_resolver,
+          "https://json-schema.org/draft/2020-12/schema")
+          .get()};
+  EXPECT_TRUE(id.has_value());
+  EXPECT_EQ(id.value(), "https://example.com/my-schema");
+}
