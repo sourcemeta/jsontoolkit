ea1adbb18997e352d2e3a87f91200c0feed4f602
Juan Cruz Viotti <jv@jviotti.com>
2023-10-25 17:50:16 -0400

    Fix documentation example for `frame()` (#331)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsonschema/include/sourcemeta/jsontoolkit/jsonschema_reference.h
+++ src/jsonschema/include/sourcemeta/jsontoolkit/jsonschema_reference.h
@@ -20,9 +20,9 @@
 namespace sourcemeta::jsontoolkit {
 
 /// @ingroup jsonschema
-/// A JSON Schema reference frame is a mapping of URIs to JSON Pointers within a
-/// schema. We call it reference frame as this mapping is essential for
-/// resolving references.
+/// A JSON Schema reference frame is a mapping of URIs to schema identifiers,
+/// JSON Pointers within the schema, and subschemas dialects. We call it
+/// reference frame as this mapping is essential for resolving references.
 using ReferenceFrame =
     std::map<std::string, std::tuple<std::string, Pointer, std::string>>;
 
@@ -30,8 +30,10 @@ using ReferenceFrame =
 
 /// @ingroup jsonschema
 ///
-/// This function computes the static reference frame of a schema.
-/// For example:
+/// This function computes the static reference frame of a schema. Each entry
+/// consists in the URI of the resulting schema, the JSON Pointer within that
+/// resulting schema, and the dialect URI of the resulting subschema. For
+/// example:
 ///
 /// ```cpp
 /// #include <sourcemeta/jsontoolkit/json.h>
@@ -55,12 +57,27 @@ using ReferenceFrame =
 ///     .wait();
 ///
 /// assert(static_frame.size() == 3);
+///
 /// assert(std::get<0>(static_frame.at("https://www.example.com/schema"))
+///   == "https://www.example.com/schema");
+/// assert(std::get<1>(static_frame.at("https://www.example.com/schema"))
 ///   == sourcemeta::jsonpointer::Pointer{});
+/// assert(std::get<2>(static_frame.at("https://www.example.com/schema"))
+///   == "https://json-schema.org/draft/2020-12/schema");
+///
 /// assert(std::get<0>(static_frame.at("https://www.example.com/foo"))
+///   == "https://www.example.com/schema");
+/// assert(std::get<1>(static_frame.at("https://www.example.com/foo"))
 ///   == sourcemeta::jsonpointer::Pointer{"items"});
+/// assert(std::get<2>(static_frame.at("https://www.example.com/foo"))
+///   == "https://json-schema.org/draft/2020-12/schema");
+///
 /// assert(std::get<0>(static_frame.at("https://www.example.com/schema#test"))
+///   == "https://www.example.com/schema");
+/// assert(std::get<1>(static_frame.at("https://www.example.com/schema#test"))
 ///   == sourcemeta::jsonpointer::Pointer{"properties", "foo"});
+/// assert(std::get<2>(static_frame.at("https://www.example.com/schema#test"))
+///   == "https://json-schema.org/draft/2020-12/schema");
 /// ```
 SOURCEMETA_JSONTOOLKIT_JSONSCHEMA_EXPORT
 auto frame(const JSON &schema, ReferenceFrame &static_frame,
