af7e437440ac88ad8338f32ff75efe278993594b
Juan Cruz Viotti <jv@jviotti.com>
2023-11-27 16:10:46 -0400

    Extend the URI class to get port numbers
    Depends-On: https://github.com/sourcemeta/jsontoolkit/pull/352
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/uri/uri_host_test.cc
+++ test/uri/uri_host_test.cc
@@ -7,6 +7,12 @@ TEST(URI_host, https_example_url) {
   EXPECT_EQ(uri.host().value(), "example.com");
 }
 
+TEST(URI_host, https_example_url_with_port) {
+  const sourcemeta::jsontoolkit::URI uri{"https://example.com:8000/foo"};
+  EXPECT_TRUE(uri.host().has_value());
+  EXPECT_EQ(uri.host().value(), "example.com");
+}
+
 TEST(URI_host, relative_url) {
   const sourcemeta::jsontoolkit::URI uri{"../foo"};
   EXPECT_FALSE(uri.host().has_value());
