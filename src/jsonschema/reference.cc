9bf14604d293d3613144408ba5f807504078a686
Juan Cruz Viotti <jv@jviotti.com>
2023-12-16 14:52:41 -0300

    Extend `.frame()` to work on multiple URI bases
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsonschema/reference.cc
+++ src/jsonschema/reference.cc
@@ -7,12 +7,13 @@
 #include <map>      // std::map
 #include <optional> // std::optional
 #include <sstream>  // std::ostringstream
+#include <vector>   // std::vector
 
-static auto
-find_base(const std::map<sourcemeta::jsontoolkit::Pointer, std::string> &bases,
-          const sourcemeta::jsontoolkit::Pointer &pointer,
-          const std::optional<std::string> &default_base)
-    -> const std::string & {
+static auto find_bases(const std::map<sourcemeta::jsontoolkit::Pointer,
+                                      std::vector<std::string>> &bases,
+                       const sourcemeta::jsontoolkit::Pointer &pointer,
+                       const std::optional<std::string> &default_base)
+    -> std::vector<std::string> {
   for (const auto &subpointer :
        sourcemeta::jsontoolkit::SubPointerWalker{pointer}) {
     if (bases.contains(subpointer)) {
@@ -21,7 +22,7 @@ find_base(const std::map<sourcemeta::jsontoolkit::Pointer, std::string> &bases,
   }
 
   assert(default_base.has_value());
-  return default_base.value();
+  return {default_base.value()};
 }
 
 auto sourcemeta::jsontoolkit::ReferenceFrame::defines(
@@ -74,8 +75,10 @@ auto sourcemeta::jsontoolkit::frame(
     const sourcemeta::jsontoolkit::SchemaResolver &resolver,
     const std::optional<std::string> &default_dialect,
     const std::optional<std::string> &default_id) -> std::future<void> {
-  std::map<sourcemeta::jsontoolkit::Pointer, std::string> base_uris;
-  std::map<sourcemeta::jsontoolkit::Pointer, std::string> base_dialects;
+  std::map<sourcemeta::jsontoolkit::Pointer, std::vector<std::string>>
+      base_uris;
+  std::map<sourcemeta::jsontoolkit::Pointer, std::vector<std::string>>
+      base_dialects;
 
   const std::optional<std::string> root_id{
       sourcemeta::jsontoolkit::id(schema, resolver, default_dialect, default_id)
@@ -90,38 +93,44 @@ auto sourcemeta::jsontoolkit::frame(
     const std::optional<std::string> current_dialect{
         sourcemeta::jsontoolkit::dialect(subschema)};
     if (current_dialect.has_value()) {
-      base_dialects.insert({pointer, current_dialect.value()});
+      base_dialects.insert({pointer, {current_dialect.value()}});
     }
 
-    const std::string effective_dialect{
-        find_base(base_dialects, pointer, root_dialect)};
+    const auto effective_dialects{
+        find_bases(base_dialects, pointer, root_dialect)};
 
     // Handle schema identifiers
     const std::optional<std::string> id{
-        sourcemeta::jsontoolkit::id(subschema, resolver, effective_dialect,
+        sourcemeta::jsontoolkit::id(subschema, resolver,
+                                    effective_dialects.front(),
                                     pointer.empty() ? default_id : std::nullopt)
             .get()};
     if (id.has_value()) {
-      const sourcemeta::jsontoolkit::URI base{
-          find_base(base_uris, pointer, id)};
-      const sourcemeta::jsontoolkit::URI relative{id.value()};
-      const std::string new_id{relative.resolve_from(base)};
-      assert(root_id.has_value());
-      static_frame.store(new_id, root_id.value(), pointer, effective_dialect);
-      base_uris.insert({pointer, new_id});
+      for (const auto &base_string : find_bases(base_uris, pointer, id)) {
+        const sourcemeta::jsontoolkit::URI base{base_string};
+        const sourcemeta::jsontoolkit::URI relative{id.value()};
+        const std::string new_id{relative.resolve_from(base)};
+        assert(root_id.has_value());
+        static_frame.store(new_id, root_id.value(), pointer,
+                           effective_dialects.front());
+        base_uris.insert({pointer, {new_id}});
+      }
     }
 
     // Handle schema anchors
-    for (const auto &[name, type] : sourcemeta::jsontoolkit::anchors(
-                                        subschema, resolver, effective_dialect)
-                                        .get()) {
+    for (const auto &[name, type] :
+         sourcemeta::jsontoolkit::anchors(subschema, resolver,
+                                          effective_dialects.front())
+             .get()) {
       const auto anchor_uri{sourcemeta::jsontoolkit::URI::from_fragment(name)};
-      const sourcemeta::jsontoolkit::URI anchor_base{
-          find_base(base_uris, pointer, id)};
-      const auto result{anchor_uri.resolve_from(anchor_base)};
-      if (type == sourcemeta::jsontoolkit::AnchorType::Static) {
-        assert(root_id.has_value());
-        static_frame.store(result, root_id.value(), pointer, effective_dialect);
+      for (const auto &base_string : find_bases(base_uris, pointer, id)) {
+        const sourcemeta::jsontoolkit::URI anchor_base{base_string};
+        const auto result{anchor_uri.resolve_from(anchor_base)};
+        if (type == sourcemeta::jsontoolkit::AnchorType::Static) {
+          assert(root_id.has_value());
+          static_frame.store(result, root_id.value(), pointer,
+                             effective_dialects.front());
+        }
       }
     }
   }
