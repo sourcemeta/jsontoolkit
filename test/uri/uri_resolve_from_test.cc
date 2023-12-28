b2c175a065d3d58a13b4dfa8c628d16110bcc2ac
Juan Cruz Viotti <jv@jviotti.com>
2023-12-28 12:09:16 -0300

    Test URI resolution with fragments (#414)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/uri/uri_resolve_from_test.cc
+++ test/uri/uri_resolve_from_test.cc
@@ -12,3 +12,15 @@ TEST(URI_resolve_from, example_1) {
   const sourcemeta::jsontoolkit::URI relative{"../baz"};
   EXPECT_EQ(relative.resolve_from(base), "https://foobar.com/baz");
 }
+
+TEST(URI_resolve_from, add_fragment) {
+  const sourcemeta::jsontoolkit::URI base{"https://foobar.com/foo/bar"};
+  const sourcemeta::jsontoolkit::URI relative{"#baz"};
+  EXPECT_EQ(relative.resolve_from(base), "https://foobar.com/foo/bar#baz");
+}
+
+TEST(URI_resolve_from, change_fragment) {
+  const sourcemeta::jsontoolkit::URI base{"https://foobar.com/foo/bar#baz"};
+  const sourcemeta::jsontoolkit::URI relative{"#qux"};
+  EXPECT_EQ(relative.resolve_from(base), "https://foobar.com/foo/bar#qux");
+}
