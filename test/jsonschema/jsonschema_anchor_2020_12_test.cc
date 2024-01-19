b07969bf3fd3cdecf021bce0c7f82d97e8f90b45
Juan Cruz Viotti <jv@jviotti.com>
2024-01-18 20:08:31 -0400

    Correctly detect anchors that are both static and dynamic (#458)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/jsonschema/jsonschema_anchor_2020_12_test.cc
+++ test/jsonschema/jsonschema_anchor_2020_12_test.cc
@@ -54,7 +54,7 @@ TEST(JSONSchema_anchor_2020_12, top_level_dynamic_anchor) {
   EXPECT_EQ(anchors.at("foo"), sourcemeta::jsontoolkit::AnchorType::Dynamic);
 }
 
-TEST(JSONSchema_anchor_2020_12, top_level_static_and_dynamic_anchor) {
+TEST(JSONSchema_anchor_2020_12, top_level_static_and_dynamic_anchor_different) {
   const sourcemeta::jsontoolkit::JSON document =
       sourcemeta::jsontoolkit::parse(R"JSON({
     "$schema": "https://json-schema.org/draft/2020-12/schema",
@@ -70,6 +70,22 @@ TEST(JSONSchema_anchor_2020_12, top_level_static_and_dynamic_anchor) {
   EXPECT_TRUE(anchors.contains("bar"));
   EXPECT_EQ(anchors.at("foo"), sourcemeta::jsontoolkit::AnchorType::Dynamic);
   EXPECT_EQ(anchors.at("bar"), sourcemeta::jsontoolkit::AnchorType::Static);
+}
+
+TEST(JSONSchema_anchor_2020_12, top_level_static_and_dynamic_anchor_same) {
+  const sourcemeta::jsontoolkit::JSON document =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "$anchor": "foo",
+    "$dynamicAnchor": "foo"
+  })JSON");
+
+  const auto anchors{sourcemeta::jsontoolkit::anchors(
+                         document, sourcemeta::jsontoolkit::official_resolver)
+                         .get()};
+  EXPECT_EQ(anchors.size(), 1);
+  EXPECT_TRUE(anchors.contains("foo"));
+  EXPECT_EQ(anchors.at("foo"), sourcemeta::jsontoolkit::AnchorType::All);
 }
 
 TEST(JSONSchema_anchor_2020_12, nested_static_with_default_dialect) {
