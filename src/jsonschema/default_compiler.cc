5ae44daf572c3da80502a4241413af6dd8a255c6
Juan Cruz Viotti <jv@jviotti.com>
2024-04-09 18:56:59 -0400

    Split default schema compiler into dialects
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsonschema/default_compiler.cc
+++ src/jsonschema/default_compiler.cc
@@ -1,49 +1,8 @@
 #include <sourcemeta/jsontoolkit/jsonschema_compile.h>
 
-#include <algorithm> // std::transform
-#include <cassert>   // assert
-#include <utility>   // std::move
+#include "default_compiler_2020_12.h"
 
-namespace {
-
-auto type_to_assertion(
-    const sourcemeta::jsontoolkit::Pointer &instance_location,
-    const sourcemeta::jsontoolkit::Pointer &schema_location,
-    const std::string &type)
-    -> sourcemeta::jsontoolkit::SchemaCompilerTemplate {
-  using namespace sourcemeta::jsontoolkit;
-  if (type == "null") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::Null, {}}};
-  } else if (type == "boolean") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::Boolean, {}}};
-  } else if (type == "object") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::Object, {}}};
-  } else if (type == "array") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::Array, {}}};
-  } else if (type == "number") {
-    return {SchemaCompilerLogicalOr{
-        schema_location,
-        {},
-        {SchemaCompilerAssertionType{
-             instance_location, schema_location, JSON::Type::Real, {}},
-         SchemaCompilerAssertionType{
-             instance_location, schema_location, JSON::Type::Integer, {}}}}};
-  } else if (type == "integer") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::Integer, {}}};
-  } else if (type == "string") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::String, {}}};
-  } else {
-    return {};
-  }
-}
-
-} // namespace
+#include <cassert> // assert
 
 auto sourcemeta::jsontoolkit::default_schema_compiler(
     const sourcemeta::jsontoolkit::SchemaCompilerContext &context)
@@ -56,26 +15,7 @@ auto sourcemeta::jsontoolkit::default_schema_compiler(
   if (context.keyword == "type" &&
       context.vocabularies.contains(
           "https://json-schema.org/draft/2020-12/vocab/validation")) {
-    if (context.value.is_string()) {
-      return type_to_assertion(context.instance_location,
-                               context.schema_location,
-                               context.value.to_string());
-    } else if (context.value.is_array()) {
-      assert(!context.value.empty());
-      SchemaCompilerTemplate disjunctors;
-      for (const auto &type : context.value.as_array()) {
-        assert(type.is_string());
-        SchemaCompilerTemplate disjunctor{
-            type_to_assertion(context.instance_location,
-                              context.schema_location, type.to_string())};
-        assert(disjunctor.size() == 1);
-        disjunctors.push_back(std::move(disjunctor).front());
-      }
-
-      assert(disjunctors.size() == context.value.size());
-      return {SchemaCompilerLogicalOr{
-          context.schema_location, {}, std::move(disjunctors)}};
-    }
+    return internal::compiler_2020_12_validation_type(context);
   }
 
   return {};
