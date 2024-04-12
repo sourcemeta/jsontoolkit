573bea8a99f80e672417c293571b9944070e4fc7
Juan Cruz Viotti <jv@jviotti.com>
2024-04-12 11:58:36 -0400

    Compile "type" for Draft 4
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsonschema/default_compiler_2020_12.h
+++ src/jsonschema/default_compiler_2020_12.h
@@ -3,77 +3,14 @@
 
 #include <sourcemeta/jsontoolkit/jsonschema_compile.h>
 
-#include <cassert> // assert
-#include <utility> // std::move
-
-namespace {
-
-auto type_to_assertion(
-    const sourcemeta::jsontoolkit::Pointer &instance_location,
-    const sourcemeta::jsontoolkit::Pointer &schema_location,
-    const std::string &type)
-    -> sourcemeta::jsontoolkit::SchemaCompilerTemplate {
-  using namespace sourcemeta::jsontoolkit;
-  if (type == "null") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::Null, {}}};
-  } else if (type == "boolean") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::Boolean, {}}};
-  } else if (type == "object") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::Object, {}}};
-  } else if (type == "array") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::Array, {}}};
-  } else if (type == "number") {
-    return {SchemaCompilerLogicalOr{
-        schema_location,
-        {},
-        {SchemaCompilerAssertionType{
-             instance_location, schema_location, JSON::Type::Real, {}},
-         SchemaCompilerAssertionType{
-             instance_location, schema_location, JSON::Type::Integer, {}}}}};
-  } else if (type == "integer") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::Integer, {}}};
-  } else if (type == "string") {
-    return {SchemaCompilerAssertionType{
-        instance_location, schema_location, JSON::Type::String, {}}};
-  } else {
-    return {};
-  }
-}
-
-} // namespace
+#include "default_compiler_draft4.h"
 
 namespace internal {
 
 auto compiler_2020_12_validation_type(
     const sourcemeta::jsontoolkit::SchemaCompilerContext &context)
     -> sourcemeta::jsontoolkit::SchemaCompilerTemplate {
-  using namespace sourcemeta::jsontoolkit;
-  if (context.value.is_string()) {
-    return type_to_assertion(context.instance_location, context.schema_location,
-                             context.value.to_string());
-  } else if (context.value.is_array()) {
-    assert(!context.value.empty());
-    SchemaCompilerTemplate disjunctors;
-    for (const auto &type : context.value.as_array()) {
-      assert(type.is_string());
-      SchemaCompilerTemplate disjunctor{
-          type_to_assertion(context.instance_location, context.schema_location,
-                            type.to_string())};
-      assert(disjunctor.size() == 1);
-      disjunctors.push_back(std::move(disjunctor).front());
-    }
-
-    assert(disjunctors.size() == context.value.size());
-    return {SchemaCompilerLogicalOr{
-        context.schema_location, {}, std::move(disjunctors)}};
-  }
-
-  return {};
+  return compiler_draft4_validation_type(context);
 }
 
 } // namespace internal
---
+++ src/jsonschema/default_compiler_draft4.h
@@ -0,0 +1,81 @@
+#ifndef SOURCEMETA_JSONTOOLKIT_JSONSCHEMA_DEFAULT_COMPILER_DRAFT4_H_
+#define SOURCEMETA_JSONTOOLKIT_JSONSCHEMA_DEFAULT_COMPILER_DRAFT4_H_
+
+#include <sourcemeta/jsontoolkit/jsonschema_compile.h>
+
+#include <cassert> // assert
+#include <utility> // std::move
+
+namespace {
+
+auto type_string_to_assertion(
+    const sourcemeta::jsontoolkit::Pointer &instance_location,
+    const sourcemeta::jsontoolkit::Pointer &schema_location,
+    const std::string &type)
+    -> sourcemeta::jsontoolkit::SchemaCompilerTemplate {
+  using namespace sourcemeta::jsontoolkit;
+  if (type == "null") {
+    return {SchemaCompilerAssertionType{
+        instance_location, schema_location, JSON::Type::Null, {}}};
+  } else if (type == "boolean") {
+    return {SchemaCompilerAssertionType{
+        instance_location, schema_location, JSON::Type::Boolean, {}}};
+  } else if (type == "object") {
+    return {SchemaCompilerAssertionType{
+        instance_location, schema_location, JSON::Type::Object, {}}};
+  } else if (type == "array") {
+    return {SchemaCompilerAssertionType{
+        instance_location, schema_location, JSON::Type::Array, {}}};
+  } else if (type == "number") {
+    return {SchemaCompilerLogicalOr{
+        schema_location,
+        {},
+        {SchemaCompilerAssertionType{
+             instance_location, schema_location, JSON::Type::Real, {}},
+         SchemaCompilerAssertionType{
+             instance_location, schema_location, JSON::Type::Integer, {}}}}};
+  } else if (type == "integer") {
+    return {SchemaCompilerAssertionType{
+        instance_location, schema_location, JSON::Type::Integer, {}}};
+  } else if (type == "string") {
+    return {SchemaCompilerAssertionType{
+        instance_location, schema_location, JSON::Type::String, {}}};
+  } else {
+    return {};
+  }
+}
+
+} // namespace
+
+namespace internal {
+
+auto compiler_draft4_validation_type(
+    const sourcemeta::jsontoolkit::SchemaCompilerContext &context)
+    -> sourcemeta::jsontoolkit::SchemaCompilerTemplate {
+  using namespace sourcemeta::jsontoolkit;
+  if (context.value.is_string()) {
+    return type_string_to_assertion(context.instance_location,
+                                    context.schema_location,
+                                    context.value.to_string());
+  } else if (context.value.is_array()) {
+    assert(!context.value.empty());
+    SchemaCompilerTemplate disjunctors;
+    for (const auto &type : context.value.as_array()) {
+      assert(type.is_string());
+      SchemaCompilerTemplate disjunctor{
+          type_string_to_assertion(context.instance_location,
+                                   context.schema_location, type.to_string())};
+      assert(disjunctor.size() == 1);
+      disjunctors.push_back(std::move(disjunctor).front());
+    }
+
+    assert(disjunctors.size() == context.value.size());
+    return {SchemaCompilerLogicalOr{
+        context.schema_location, {}, std::move(disjunctors)}};
+  }
+
+  return {};
+}
+
+} // namespace internal
+#endif
