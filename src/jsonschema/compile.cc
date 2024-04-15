f7c4e0b8a5b3c42d79005a1bf3080d7e2ab21bde
Juan Cruz Viotti <jv@jviotti.com>
2024-04-15 14:08:42 -0400

    Add base and relative pointer to compiler context (#541)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsonschema/compile.cc
+++ src/jsonschema/compile.cc
@@ -33,6 +33,7 @@ auto compile_subschema(
     const auto &keyword{entry.pointer.back().to_property()};
     for (auto &&step : context.compiler(
              {keyword, context.schema, entry.vocabularies, entry.value,
+              context.base, context.relative_pointer.concat({keyword}),
               context.evaluation_path.concat({keyword}),
               context.instance_location, context.frame, context.references,
               context.walker, context.resolver, context.compiler,
@@ -77,6 +78,8 @@ auto compile(const JSON &schema, const SchemaWalker &walker,
        result,
        vocabularies(schema, resolver, default_dialect).get(),
        result,
+       root_frame_entry.base,
+       {},
        {},
        {},
        frame,
@@ -89,12 +92,25 @@ auto compile(const JSON &schema, const SchemaWalker &walker,
 
 auto compile(const SchemaCompilerContext &context,
              const Pointer &pointer) -> SchemaCompilerTemplate {
+  // Determine URI of the destination after recursion
+  URI destination_uri{to_uri(context.relative_pointer.concat(pointer))};
+  if (context.base.is_absolute()) {
+    destination_uri.resolve_from(context.base);
+  }
+  const std::string destination{destination_uri.canonicalize().recompose()};
+
+  // Otherwise the recursion attempt is non-sense
+  assert(context.frame.contains({ReferenceType::Static, destination}));
+  const auto &entry{context.frame.at({ReferenceType::Static, destination})};
+
+  const auto &new_schema{get(context.value, pointer)};
   return compile_subschema(
-      {context.keyword, get(context.value, pointer), context.vocabularies,
-       context.value, context.evaluation_path.concat(pointer),
-       context.instance_location, context.frame, context.references,
-       context.walker, context.resolver, context.compiler,
-       context.default_dialect});
+      {context.keyword, new_schema,
+       vocabularies(new_schema, context.resolver, entry.dialect).get(),
+       context.value, entry.base, entry.relative_pointer,
+       context.evaluation_path.concat(pointer), context.instance_location,
+       context.frame, context.references, context.walker, context.resolver,
+       context.compiler, entry.dialect});
 }
 
 } // namespace sourcemeta::jsontoolkit
