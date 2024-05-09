6ccdd9843c7229263971356d3a12e19916ae3429
Juan Cruz Viotti <jv@jviotti.com>
2024-05-08 20:05:04 -0400

    Enable `/MP` for faster MSVC builds (#685)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- cmake/CompilerOptions.cmake
+++ cmake/CompilerOptions.cmake
@@ -6,6 +6,7 @@ function(sourcemeta_jsontoolkit_add_compile_options target)
       /permissive-
       /W4
       /WL
+      /MP
       /sdl)
   else()
     target_compile_options("${target}" PRIVATE
