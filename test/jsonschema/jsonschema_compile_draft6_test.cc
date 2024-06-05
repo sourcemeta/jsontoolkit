02e7dd1bbd123363005a38e303c05d1b5540fbca
Juan Cruz Viotti <jv@jviotti.com>
2024-06-05 11:55:56 -0400

    Compile `exclusiveMinimum` and `exclusiveMaximum` for Draft 6
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/jsonschema/jsonschema_compile_draft6_test.cc
+++ test/jsonschema/jsonschema_compile_draft6_test.cc
@@ -54,3 +54,87 @@ TEST(JSONSchema_compile_draft6, const_2) {
   EVALUATE_WITH_TRACE_FAST_FAILURE(compiled_schema, instance, 1);
   EVALUATE_TRACE_FAILURE(0, AssertionEqual, "/const", "#/const", "");
 }
+
+TEST(JSONSchema_compile_draft6, exclusiveMinimum_1) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "exclusiveMinimum": 2
+  })JSON")};
+
+  const auto compiled_schema{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  const sourcemeta::jsontoolkit::JSON instance{2.1};
+  EVALUATE_WITH_TRACE_FAST_SUCCESS(compiled_schema, instance, 1);
+  EVALUATE_TRACE_SUCCESS(0, AssertionGreater, "/exclusiveMinimum",
+                         "#/exclusiveMinimum", "");
+
+  EVALUATE_TRACE_DESCRIBE(
+      0, "The target number is expected to be greater than the given number");
+}
+
+TEST(JSONSchema_compile_draft6, exclusiveMinimum_2) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "exclusiveMinimum": 2
+  })JSON")};
+
+  const auto compiled_schema{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  const sourcemeta::jsontoolkit::JSON instance{2};
+  EVALUATE_WITH_TRACE_FAST_FAILURE(compiled_schema, instance, 1);
+  EVALUATE_TRACE_FAILURE(0, AssertionGreater, "/exclusiveMinimum",
+                         "#/exclusiveMinimum", "");
+
+  EVALUATE_TRACE_DESCRIBE(
+      0, "The target number is expected to be greater than the given number");
+}
+
+TEST(JSONSchema_compile_draft6, exclusiveMaximum_1) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "exclusiveMaximum": 2
+  })JSON")};
+
+  const auto compiled_schema{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  const sourcemeta::jsontoolkit::JSON instance{1.9};
+  EVALUATE_WITH_TRACE_FAST_SUCCESS(compiled_schema, instance, 1);
+  EVALUATE_TRACE_SUCCESS(0, AssertionLess, "/exclusiveMaximum",
+                         "#/exclusiveMaximum", "");
+
+  EVALUATE_TRACE_DESCRIBE(
+      0, "The target number is expected to be less than the given number");
+}
+
+TEST(JSONSchema_compile_draft6, exclusiveMaximum_2) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-06/schema#",
+    "exclusiveMaximum": 2
+  })JSON")};
+
+  const auto compiled_schema{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  const sourcemeta::jsontoolkit::JSON instance{2};
+  EVALUATE_WITH_TRACE_FAST_FAILURE(compiled_schema, instance, 1);
+  EVALUATE_TRACE_FAILURE(0, AssertionLess, "/exclusiveMaximum",
+                         "#/exclusiveMaximum", "");
+
+  EVALUATE_TRACE_DESCRIBE(
+      0, "The target number is expected to be less than the given number");
+}
