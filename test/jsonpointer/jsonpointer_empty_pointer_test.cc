475e6d3c1bc5ec6d844d778164ec1a4a2d9253d5
Juan Cruz Viotti <jv@jviotti.com>
2024-06-06 14:48:09 -0400

    Remove other stuff
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/jsonpointer/jsonpointer_empty_pointer_test.cc
+++
@@ -1,8 +0,0 @@
-#include <gtest/gtest.h>
-#include <sourcemeta/jsontoolkit/jsonpointer.h>
-
-TEST(JSONPointer_empty_pointer, is_empty) {
-  EXPECT_TRUE(sourcemeta::jsontoolkit::empty_pointer.empty());
-  EXPECT_EQ(sourcemeta::jsontoolkit::empty_pointer,
-            sourcemeta::jsontoolkit::Pointer{});
-}
