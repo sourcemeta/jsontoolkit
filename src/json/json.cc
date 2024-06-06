e974b8e9804b1ce56749f675d678af3dbc3e2cf9
Juan Cruz Viotti <jv@jviotti.com>
2024-06-06 14:07:34 -0400

    Simplify
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/json/json.cc
+++ src/json/json.cc
@@ -2,26 +2,12 @@
 
 #include <sourcemeta/jsontoolkit/json.h>
 
-#include <cassert> // assert
-#include <fstream> // std::ifstream
-#include <ios>     // std::ios_base
-
 namespace sourcemeta::jsontoolkit {
-
-auto parse(std::basic_istream<JSON::Char, JSON::CharTraits> &stream,
-           std::uint64_t &line, std::uint64_t &column) -> JSON {
-  return internal_parse(stream, line, column);
-}
-
-auto parse(const std::basic_string<JSON::Char, JSON::CharTraits> &input,
-           std::uint64_t &line, std::uint64_t &column) -> JSON {
-  return internal_parse(input, line, column);
-}
 
 auto parse(std::basic_istream<JSON::Char, JSON::CharTraits> &stream) -> JSON {
   std::uint64_t line{1};
   std::uint64_t column{0};
-  return parse(stream, line, column);
+  return internal_parse(stream, line, column);
 }
 
 } // namespace sourcemeta::jsontoolkit
