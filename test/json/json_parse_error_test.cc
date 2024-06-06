706c474d38f4c116c930a1a69808d4893f9a7be6
Juan Cruz Viotti <jv@jviotti.com>
2024-06-06 14:42:10 -0400

    Even simpler
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/json/json_parse_error_test.cc
+++ test/json/json_parse_error_test.cc
@@ -1,10 +1,5 @@
-#include <exception>
 #include <gtest/gtest.h>
-#include <sstream>
-
 #include <iostream>
-
-#include <sourcemeta/jsontoolkit/json.h>
 
 TEST(JSON_parse_error, boolean_true_invalid) {
   std::cerr << "JSON_parse_error.boolean_true_invalid\n";
