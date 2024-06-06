6b1701b4116b4808ee95e2894618eb2564292ba6
Juan Cruz Viotti <jv@jviotti.com>
2024-06-06 14:39:24 -0400

    Try very simple
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/json/json_parse_error_test.cc
+++ test/json/json_parse_error_test.cc
@@ -6,25 +6,7 @@
 
 #include <sourcemeta/jsontoolkit/json.h>
 
-#define EXPECT_PARSE_ERROR(input, expected_line, expected_column)              \
-  try {                                                                        \
-    sourcemeta::jsontoolkit::parse((input));                                   \
-    std::cerr << "WENT BEYOND PARSE\n"; \
-    FAIL() << "The parse function was expected to throw";                      \
-  } catch (const sourcemeta::jsontoolkit::ParseError &error) {                 \
-    std::cerr << "PARSE_ERROR\n"; \
-    EXPECT_EQ(error.line(), expected_line);                                    \
-    EXPECT_EQ(error.column(), expected_column);                                \
-    std::cerr << "SUCCEED!\n"; \
-    SUCCEED();                                                                 \
-  } catch (const std::exception &) {                                           \
-    std::cerr << "GENERAL EXCEPTION\n"; \
-    FAIL() << "The parse function was expected to throw a parse error";        \
-  }
-
 TEST(JSON_parse_error, boolean_true_invalid) {
   std::cerr << "JSON_parse_error.boolean_true_invalid\n";
-  std::istringstream input{"trrue"};
-  std::cerr << "After std::istringstream\n";
-  EXPECT_PARSE_ERROR(input, 1, 3);
+  SUCCEED();
 }
