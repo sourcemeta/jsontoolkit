ecc4154842e2ea578547d9e376f23fc1b0c12637
Juan Cruz Viotti <jv@jviotti.com>
2024-06-06 20:14:46 -0400

    Simpler
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test.cc
+++ test.cc
@@ -1,14 +1,12 @@
 #include <future>
 #include <optional>
 #include <string>
-
 #include <iostream>
 
 int main() {
-  std::cerr << "++++ dialect has no value\n";
   std::promise<std::optional<std::string>> promise;
-  std::cerr << "++++ setting promise value\n";
+  std::cerr << "Setting promise value\n";
   promise.set_value(std::nullopt);
-  std::cerr << "XXXX Got id\n";
+  std::cerr << "Exitting\n";
   return 0;
 }
