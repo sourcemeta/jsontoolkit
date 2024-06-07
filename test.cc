466d26bec4d6348ad358b6f8220e2b07fd530d4d
Juan Cruz Viotti <jv@jviotti.com>
2024-06-06 20:11:30 -0400

    Even simpler
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test.cc
+++ test.cc
@@ -4,19 +4,11 @@
 
 #include <iostream>
 
-auto base_dialect() -> std::future<std::optional<std::string>> {
+int main() {
   std::cerr << "++++ dialect has no value\n";
   std::promise<std::optional<std::string>> promise;
   std::cerr << "++++ setting promise value\n";
   promise.set_value(std::nullopt);
-  std::cerr << "++++ returning\n";
-  return promise.get_future();
-}
-
-int main() {
-  std::cerr << "XXXX JSONSchema_id_draft4.id_boolean_default_dialect\n";
-  std::cerr << "XXXX Getting id\n";
-  base_dialect().wait();
   std::cerr << "XXXX Got id\n";
   return 0;
 }
