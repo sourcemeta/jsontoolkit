d5f3d07b0eb94c9befb03c6ea1d49c2e18cd0de5
Juan Cruz Viotti <jv@jviotti.com>
2024-06-06 20:17:01 -0400

    Int
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test.cc
+++ test.cc
@@ -4,9 +4,9 @@
 #include <iostream>
 
 int main() {
-  std::promise<std::optional<int>> promise;
+  std::promise<int> promise;
   std::cerr << "Setting promise value\n";
-  promise.set_value(std::nullopt);
+  promise.set_value(5);
   std::cerr << "Exitting\n";
   return 0;
 }
