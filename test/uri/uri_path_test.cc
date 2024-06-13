25f9c86fff25d022f18b491e583216ed64f74a49
Tony Gorez <gorez.tony@gmail.com>
2024-06-13 11:47:48 +0200

    test: re-enable uri_path tests
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/uri/uri_path_test.cc
+++ test/uri/uri_path_test.cc
@@ -24,48 +24,48 @@ TEST(URI_path, https_example_url_slash) {
   EXPECT_SPAN_EQ(uri.path().value(), {""});
 }
 
-// TEST(URI_path, https_example_url_single) {
-//   const sourcemeta::jsontoolkit::URI uri{"https://example.com/foo"};
-//   EXPECT_TRUE(uri.path().has_value());
-//   EXPECT_EQ(uri.path().value(), "/foo");
-// }
-//
-// TEST(URI_path, https_example_url_multi) {
-//   const sourcemeta::jsontoolkit::URI uri{"https://example.com/foo/bar/baz"};
-//   EXPECT_TRUE(uri.path().has_value());
-//   EXPECT_EQ(uri.path().value(), "/foo/bar/baz");
-// }
-//
-// TEST(URI_path, relative_multi) {
-//   const sourcemeta::jsontoolkit::URI uri{"../foo/bar"};
-//   EXPECT_TRUE(uri.path().has_value());
-//   EXPECT_EQ(uri.path().value(), "/../foo/bar");
-// }
-//
-// TEST(URI_path, urn) {
-//   const sourcemeta::jsontoolkit::URI uri{"urn:example:schema"};
-//   EXPECT_TRUE(uri.path().has_value());
-//   EXPECT_EQ(uri.path().value(), "example:schema");
-// }
-//
-// TEST(URI_path, urn_with_fragment) {
-//   const sourcemeta::jsontoolkit::URI uri{"urn:example:schema#foo"};
-//   EXPECT_TRUE(uri.path().has_value());
-//   EXPECT_EQ(uri.path().value(), "example:schema");
-// }
-//
-// TEST(URI_path, tag) {
-//   const sourcemeta::jsontoolkit::URI uri{
-//       "tag:bowtie.report,2023-11:referencing-suite-tag-uris-id"};
-//   EXPECT_TRUE(uri.path().has_value());
-//   EXPECT_EQ(uri.path().value(),
-//             "bowtie.report,2023-11:referencing-suite-tag-uris-id");
-// }
-//
-// TEST(URI_path, tag_with_fragment) {
-//   const sourcemeta::jsontoolkit::URI uri{
-//       "tag:bowtie.report,2023-11:referencing-suite-tag-uris-id#foo"};
-//   EXPECT_TRUE(uri.path().has_value());
-//   EXPECT_EQ(uri.path().value(),
-//             "bowtie.report,2023-11:referencing-suite-tag-uris-id");
-// }
+TEST(URI_path, https_example_url_single) {
+  const sourcemeta::jsontoolkit::URI uri{"https://example.com/foo"};
+  EXPECT_TRUE(uri.path().has_value());
+  EXPECT_SPAN_EQ(uri.path().value(), {"foo"});
+}
+
+TEST(URI_path, https_example_url_multi) {
+  const sourcemeta::jsontoolkit::URI uri{"https://example.com/foo/bar/baz"};
+  EXPECT_TRUE(uri.path().has_value());
+  EXPECT_SPAN_EQ(uri.path().value(), {"foo", "bar", "baz"});
+}
+
+TEST(URI_path, relative_multi) {
+  const sourcemeta::jsontoolkit::URI uri{"../foo/bar"};
+  EXPECT_TRUE(uri.path().has_value());
+  EXPECT_SPAN_EQ(uri.path().value(), {"..", "foo", "bar"});
+}
+
+TEST(URI_path, urn) {
+  const sourcemeta::jsontoolkit::URI uri{"urn:example:schema"};
+  EXPECT_TRUE(uri.path().has_value());
+  EXPECT_SPAN_EQ(uri.path().value(), {"example:schema"});
+}
+
+TEST(URI_path, urn_with_fragment) {
+  const sourcemeta::jsontoolkit::URI uri{"urn:example:schema#foo"};
+  EXPECT_TRUE(uri.path().has_value());
+  EXPECT_SPAN_EQ(uri.path().value(), {"example:schema"});
+}
+
+TEST(URI_path, tag) {
+  const sourcemeta::jsontoolkit::URI uri{
+      "tag:bowtie.report,2023-11:referencing-suite-tag-uris-id"};
+  EXPECT_TRUE(uri.path().has_value());
+  EXPECT_SPAN_EQ(uri.path().value(),
+                 {"bowtie.report,2023-11:referencing-suite-tag-uris-id"});
+}
+
+TEST(URI_path, tag_with_fragment) {
+  const sourcemeta::jsontoolkit::URI uri{
+      "tag:bowtie.report,2023-11:referencing-suite-tag-uris-id#foo"};
+  EXPECT_TRUE(uri.path().has_value());
+  EXPECT_SPAN_EQ(uri.path().value(),
+                 {"bowtie.report,2023-11:referencing-suite-tag-uris-id"});
+}
