9f651df5c1e5b6b2e05abc09f1580aa11ec69c3c
Juan Cruz Viotti <jv@jviotti.com>
2024-08-12 18:15:21 -0400

    Compile a starting version of `contains` for 2020-12
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/jsonschema/jsonschema_compile_2020_12_test.cc
+++ test/jsonschema/jsonschema_compile_2020_12_test.cc
@@ -503,3 +503,202 @@ TEST(JSONSchema_compile_2020_12, prefixItems_6) {
   EVALUATE_TRACE_POST_DESCRIBE(
       3, "The target is expected to match all of the given assertions");
 }
+
+TEST(JSONSchema_compile_2020_12, contains_1) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "contains": { "type": "string" }
+  })JSON")};
+
+  const auto compiled_schema{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  const sourcemeta::jsontoolkit::JSON instance{2};
+  EVALUATE_WITH_TRACE_FAST_SUCCESS(compiled_schema, instance, 0);
+}
+
+TEST(JSONSchema_compile_2020_12, contains_2) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "contains": { "type": "string" }
+  })JSON")};
+
+  const auto compiled_schema{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  const sourcemeta::jsontoolkit::JSON instance{
+      sourcemeta::jsontoolkit::parse("[ 1, \"bar\", 3 ]")};
+  EVALUATE_WITH_TRACE_FAST_SUCCESS(compiled_schema, instance, 5);
+
+  EVALUATE_TRACE_PRE(0, LoopContains, "/contains", "#/contains", "");
+  EVALUATE_TRACE_PRE(1, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/0");
+  EVALUATE_TRACE_PRE(2, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/1");
+  EVALUATE_TRACE_PRE_ANNOTATION_PUBLIC(3, "/contains", "#/contains", "");
+  EVALUATE_TRACE_PRE(4, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/2");
+
+  EVALUATE_TRACE_POST_FAILURE(0, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/0");
+  EVALUATE_TRACE_POST_SUCCESS(1, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/1");
+  EVALUATE_TRACE_POST_ANNOTATION_PUBLIC(2, "/contains", "#/contains", "", 1);
+  EVALUATE_TRACE_POST_FAILURE(3, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/2");
+  EVALUATE_TRACE_POST_SUCCESS(4, LoopContains, "/contains", "#/contains", "");
+
+  EVALUATE_TRACE_POST_DESCRIBE(
+      0, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      1, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(2, "Emit an annotation");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      3, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      4, "A certain number of array items must satisfy the given constraints");
+}
+
+TEST(JSONSchema_compile_2020_12, contains_3) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "contains": { "type": "string" }
+  })JSON")};
+
+  const auto compiled_schema{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  const sourcemeta::jsontoolkit::JSON instance{
+      sourcemeta::jsontoolkit::parse("[ 1, 2, 3 ]")};
+  EVALUATE_WITH_TRACE_FAST_FAILURE(compiled_schema, instance, 4);
+
+  EVALUATE_TRACE_PRE(0, LoopContains, "/contains", "#/contains", "");
+  EVALUATE_TRACE_PRE(1, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/0");
+  EVALUATE_TRACE_PRE(2, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/1");
+  EVALUATE_TRACE_PRE(3, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/2");
+
+  EVALUATE_TRACE_POST_FAILURE(0, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/0");
+  EVALUATE_TRACE_POST_FAILURE(1, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/1");
+  EVALUATE_TRACE_POST_FAILURE(2, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/2");
+  EVALUATE_TRACE_POST_FAILURE(3, LoopContains, "/contains", "#/contains", "");
+
+  EVALUATE_TRACE_POST_DESCRIBE(
+      0, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      1, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      2, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      3, "A certain number of array items must satisfy the given constraints");
+}
+
+TEST(JSONSchema_compile_2020_12, contains_4) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "contains": { "type": "string" }
+  })JSON")};
+
+  const auto compiled_schema{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  const sourcemeta::jsontoolkit::JSON instance{
+      sourcemeta::jsontoolkit::parse("[ \"foo\", \"bar\", \"baz\" ]")};
+  EVALUATE_WITH_TRACE_FAST_SUCCESS(compiled_schema, instance, 7);
+
+  EVALUATE_TRACE_PRE(0, LoopContains, "/contains", "#/contains", "");
+  EVALUATE_TRACE_PRE(1, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/0");
+  EVALUATE_TRACE_PRE_ANNOTATION_PUBLIC(2, "/contains", "#/contains", "");
+  EVALUATE_TRACE_PRE(3, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/1");
+  EVALUATE_TRACE_PRE_ANNOTATION_PUBLIC(4, "/contains", "#/contains", "");
+  EVALUATE_TRACE_PRE(5, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/2");
+  EVALUATE_TRACE_PRE_ANNOTATION_PUBLIC(6, "/contains", "#/contains", "");
+
+  // TODO: We should be emitting "true" by the end of this as all items matched
+
+  EVALUATE_TRACE_POST_SUCCESS(0, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/0");
+  EVALUATE_TRACE_POST_ANNOTATION_PUBLIC(1, "/contains", "#/contains", "", 0);
+  EVALUATE_TRACE_POST_SUCCESS(2, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/1");
+  EVALUATE_TRACE_POST_ANNOTATION_PUBLIC(3, "/contains", "#/contains", "", 1);
+  EVALUATE_TRACE_POST_SUCCESS(4, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/2");
+  EVALUATE_TRACE_POST_ANNOTATION_PUBLIC(5, "/contains", "#/contains", "", 2);
+  EVALUATE_TRACE_POST_SUCCESS(6, LoopContains, "/contains", "#/contains", "");
+
+  EVALUATE_TRACE_POST_DESCRIBE(
+      0, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(1, "Emit an annotation");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      2, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(3, "Emit an annotation");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      4, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(5, "Emit an annotation");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      6, "A certain number of array items must satisfy the given constraints");
+}
+
+TEST(JSONSchema_compile_2020_12, contains_5) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "maxContains": 1,
+    "contains": { "type": "string" }
+  })JSON")};
+
+  const auto compiled_schema{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  const sourcemeta::jsontoolkit::JSON instance{
+      sourcemeta::jsontoolkit::parse("[ \"foo\", \"bar\", \"baz\" ]")};
+  EVALUATE_WITH_TRACE_FAST_FAILURE(compiled_schema, instance, 5);
+
+  EVALUATE_TRACE_PRE(0, LoopContains, "/contains", "#/contains", "");
+  EVALUATE_TRACE_PRE(1, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/0");
+  EVALUATE_TRACE_PRE_ANNOTATION_PUBLIC(2, "/contains", "#/contains", "");
+  EVALUATE_TRACE_PRE(3, AssertionTypeStrict, "/contains/type",
+                     "#/contains/type", "/1");
+  EVALUATE_TRACE_PRE_ANNOTATION_PUBLIC(4, "/contains", "#/contains", "");
+
+  EVALUATE_TRACE_POST_SUCCESS(0, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/0");
+  EVALUATE_TRACE_POST_ANNOTATION_PUBLIC(1, "/contains", "#/contains", "", 0);
+  EVALUATE_TRACE_POST_SUCCESS(2, AssertionTypeStrict, "/contains/type",
+                              "#/contains/type", "/1");
+  EVALUATE_TRACE_POST_ANNOTATION_PUBLIC(3, "/contains", "#/contains", "", 1);
+  EVALUATE_TRACE_POST_FAILURE(4, LoopContains, "/contains", "#/contains", "");
+
+  EVALUATE_TRACE_POST_DESCRIBE(
+      0, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(1, "Emit an annotation");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      2, "The target document is expected to be of the given type");
+  EVALUATE_TRACE_POST_DESCRIBE(3, "Emit an annotation");
+  EVALUATE_TRACE_POST_DESCRIBE(
+      4, "A certain number of array items must satisfy the given constraints");
+}
