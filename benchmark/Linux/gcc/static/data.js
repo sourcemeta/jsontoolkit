e74479f4ddaf81a00084562f9497ea03914e3ec5
github-action-benchmark <github@users.noreply.github.com>
2024-08-28 20:33:09 +0000

    add Benchmark (googlecpp) benchmark result for cafc42ce8e704b669fb7d59fbec53976c64446ad

--- benchmark/Linux/gcc/static/data.js
+++ benchmark/Linux/gcc/static/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724873487524,
+  "lastUpdate": 1724877189792,
   "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
   "entries": {
     "Benchmark": [
@@ -106,6 +106,42 @@ window.BENCHMARK_DATA = {
             "value": 350607.494494495,
             "unit": "ns/iter",
             "extra": "iterations: 1998\ncpu: 350587.92692692694 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "cafc42ce8e704b669fb7d59fbec53976c64446ad",
+          "message": "Introduce basic tracing facilities for schema evaluation (#1043)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-08-28T16:26:42-04:00",
+          "tree_id": "4a06b1e17f1b7f5edbe5f4b1472e2055ce611cba",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/cafc42ce8e704b669fb7d59fbec53976c64446ad"
+        },
+        "date": 1724877189222,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSONSchema_Validate_Draft4_Meta_1_No_Callback",
+            "value": 18745.25477487957,
+            "unit": "ns/iter",
+            "extra": "iterations: 37802\ncpu: 18744.50608433416 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Compile_Basic",
+            "value": 352460.5168257091,
+            "unit": "ns/iter",
+            "extra": "iterations: 1991\ncpu: 352448.05173279764 ns\nthreads: 1"
           }
         ]
       }
