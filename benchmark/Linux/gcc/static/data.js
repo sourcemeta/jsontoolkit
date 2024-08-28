cd8abc0d900df32a40001d7e382e8c809b052f9c
github-action-benchmark <github@users.noreply.github.com>
2024-08-28 23:46:03 +0000

    add Benchmark (googlecpp) benchmark result for 4d3a012bc641dbaceb845b7f9485c43a27ad6b5c

--- benchmark/Linux/gcc/static/data.js
+++ benchmark/Linux/gcc/static/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724880530099,
+  "lastUpdate": 1724888763676,
   "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
   "entries": {
     "Benchmark": [
@@ -178,6 +178,42 @@ window.BENCHMARK_DATA = {
             "value": 356278.94557478145,
             "unit": "ns/iter",
             "extra": "iterations: 1966\ncpu: 356253.01729399787 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4d3a012bc641dbaceb845b7f9485c43a27ad6b5c",
+          "message": "Walk `properties` after `required` from Draft 4 on-wards (#1046)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-08-28T19:39:05-04:00",
+          "tree_id": "5130551b7841e05d98c7df43b1e047127f9e5eb9",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/4d3a012bc641dbaceb845b7f9485c43a27ad6b5c"
+        },
+        "date": 1724888763200,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSONSchema_Validate_Draft4_Meta_1_No_Callback",
+            "value": 18230.500941820053,
+            "unit": "ns/iter",
+            "extra": "iterations: 37693\ncpu: 18229.520123099785 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Compile_Basic",
+            "value": 390551.0577557628,
+            "unit": "ns/iter",
+            "extra": "iterations: 1818\ncpu: 390520.2700770076 ns\nthreads: 1"
           }
         ]
       }
