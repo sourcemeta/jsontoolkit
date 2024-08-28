109de5cfafb6bd3b11e5b25fa611a5b33660aebc
github-action-benchmark <github@users.noreply.github.com>
2024-08-28 19:31:27 +0000

    add Benchmark (googlecpp) benchmark result for b28c217945a08b206314a0b619e990b6dcba9c70

--- benchmark/Linux/gcc/static/data.js
+++ benchmark/Linux/gcc/static/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724872105773,
+  "lastUpdate": 1724873487524,
   "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
   "entries": {
     "Benchmark": [
@@ -70,6 +70,42 @@ window.BENCHMARK_DATA = {
             "value": 351034.6524573834,
             "unit": "ns/iter",
             "extra": "iterations: 1994\ncpu: 351022.8009027083 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b28c217945a08b206314a0b619e990b6dcba9c70",
+          "message": "Get rid of weird trailing spaces in `benchmark/CMakeLists.txt` (#1044)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-08-28T15:25:03-04:00",
+          "tree_id": "d8629308cabea67e975197f85d2e631b4409d157",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/b28c217945a08b206314a0b619e990b6dcba9c70"
+        },
+        "date": 1724873486650,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSONSchema_Validate_Draft4_Meta_1_No_Callback",
+            "value": 18210.794559558155,
+            "unit": "ns/iter",
+            "extra": "iterations: 36872\ncpu: 18208.924604035583 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Compile_Basic",
+            "value": 350607.494494495,
+            "unit": "ns/iter",
+            "extra": "iterations: 1998\ncpu: 350587.92692692694 ns\nthreads: 1"
           }
         ]
       }
