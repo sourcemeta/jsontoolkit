404db3d262862f3187cb50249bf48314db1463d4
github-action-benchmark <github@users.noreply.github.com>
2024-08-28 18:08:35 +0000

    add Benchmark (googlecpp) benchmark result for 2e43ba5f786c9306c94da0d27f70fa11f636b0bc

---
+++ benchmark/macOS/clang/static/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1724868515824,
+  "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2e43ba5f786c9306c94da0d27f70fa11f636b0bc",
+          "message": "Use nicer OS names on benchmark results (#1042)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-08-28T14:05:10-04:00",
+          "tree_id": "1bb8bc49da6c3d70267297e28d10782ce4e04f19",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/2e43ba5f786c9306c94da0d27f70fa11f636b0bc"
+        },
+        "date": 1724868514413,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSONSchema_Compile_Basic",
+            "value": 206111.60311614248,
+            "unit": "ns/iter",
+            "extra": "iterations: 3530\ncpu: 200448.15864022658 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Validate_Draft4_Meta_1_No_Callback",
+            "value": 13122.366307414713,
+            "unit": "ns/iter",
+            "extra": "iterations: 53537\ncpu: 13100.640678409336 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
