596331deee7bc5d1d0fdb5ee4099cf66f44132fe
github-action-benchmark <github@users.noreply.github.com>
2024-08-28 19:31:32 +0000

    add Benchmark (googlecpp) benchmark result for b28c217945a08b206314a0b619e990b6dcba9c70

--- benchmark/Linux/gcc/shared/data.js
+++ benchmark/Linux/gcc/shared/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724872086576,
+  "lastUpdate": 1724873492614,
   "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
   "entries": {
     "Benchmark": [
@@ -70,6 +70,42 @@ window.BENCHMARK_DATA = {
             "value": 17382.92976454101,
             "unit": "ns/iter",
             "extra": "iterations: 39752\ncpu: 17382.734403300467 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b28c217945a08b206314a0b619e990b6dcba9c70",
+          "message": "Get rid of weird trailing spaces in `benchmark/CMakeLists.txt` (#1044)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-08-28T15:25:03-04:00",
+          "tree_id": "d8629308cabea67e975197f85d2e631b4409d157",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/b28c217945a08b206314a0b619e990b6dcba9c70"
+        },
+        "date": 1724873492273,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSONSchema_Compile_Basic",
+            "value": 329139.9097779946,
+            "unit": "ns/iter",
+            "extra": "iterations: 2117\ncpu: 329136.2267359471 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Validate_Draft4_Meta_1_No_Callback",
+            "value": 17745.29067836562,
+            "unit": "ns/iter",
+            "extra": "iterations: 40347\ncpu: 17744.870027511337 ns\nthreads: 1"
           }
         ]
       }
