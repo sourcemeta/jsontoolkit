5d3bd840e56ed31d1502cc693c13c3a1669c530a
github-action-benchmark <github@users.noreply.github.com>
2024-08-29 02:12:32 +0000

    add Benchmark Linux (gcc/static) (googlecpp) benchmark result for 66a594a329330d28dced59b8937e360c4101b240

--- benchmark/Linux/gcc/static/data.js
+++ benchmark/Linux/gcc/static/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724896233692,
+  "lastUpdate": 1724897552010,
   "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
   "entries": {
     "Benchmark Linux (gcc/static)": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 392662.48794165277,
             "unit": "ns/iter",
             "extra": "iterations: 1783\ncpu: 392664.74369040935 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "66a594a329330d28dced59b8937e360c4101b240",
+          "message": "Reduce `maximum`/`minimum` conditions from 3 steps to 1 (#1051)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-08-28T22:06:23-04:00",
+          "tree_id": "cc568b45811886ef14294cd7d102844d169ddeb6",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/66a594a329330d28dced59b8937e360c4101b240"
+        },
+        "date": 1724897551709,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSONSchema_Validate_Draft4_Meta_1_No_Callback",
+            "value": 18332.261125481884,
+            "unit": "ns/iter",
+            "extra": "iterations: 37886\ncpu: 18331.427809745026 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Validate_Draft4_Required_Properties",
+            "value": 10275.801177249505,
+            "unit": "ns/iter",
+            "extra": "iterations: 67955\ncpu: 10275.572231623864 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Compile_Basic",
+            "value": 391163.5914941221,
+            "unit": "ns/iter",
+            "extra": "iterations: 1787\ncpu: 391154.8724118633 ns\nthreads: 1"
           }
         ]
       }
