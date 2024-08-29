96de201c1a4829c7f4c0df57042ad3d2b197b652
Juan Cruz Viotti <jv@jviotti.com>
2024-08-28 21:27:31 -0400

    Print nanoseconds in the validation profile helper
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/validator_helper.cc
+++ benchmark/validator_helper.cc
@@ -4,6 +4,7 @@
 // This is just a convenience script to run validation under a profiler,
 // without the additional overhead of Google Benchmark.
 
+#include <chrono>     // std::chrono
 #include <cstdlib>    // EXIT_SUCCESS, EXIT_FAILURE
 #include <filesystem> // std::filesystem::path
 #include <iostream>   // std::cerr
@@ -22,7 +23,12 @@ auto main(int argc, char **argv) noexcept -> int {
       sourcemeta::jsontoolkit::official_resolver,
       sourcemeta::jsontoolkit::default_schema_compiler)};
 
+  const auto timestamp_start{std::chrono::high_resolution_clock::now()};
   const auto result{
       sourcemeta::jsontoolkit::evaluate(schema_template, instance)};
+  const auto timestamp_end{std::chrono::high_resolution_clock::now()};
+  const auto duration{std::chrono::duration_cast<std::chrono::nanoseconds>(
+      timestamp_end - timestamp_start)};
+  std::cout << duration.count() << "ns\n";
   return result ? EXIT_SUCCESS : EXIT_FAILURE;
 }
