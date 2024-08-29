7f4a87cbb2669afd6de92733b48381943c638d6b
github-action-benchmark <github@users.noreply.github.com>
2024-08-29 03:24:38 +0000

    add Benchmark (macos) (googlecpp) benchmark result for 80c122533f0ad7d3cea419889792bb8576364110

---
+++ benchmark/macos/data.js
@@ -0,0 +1,50 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1724901878797,
+  "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
+  "entries": {
+    "Benchmark (macos)": [
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80c122533f0ad7d3cea419889792bb8576364110",
+          "message": "Fix benchmark names in the GitHub Actions matrix (#1055)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-08-28T23:19:48-04:00",
+          "tree_id": "66d00469fedb1af257cd6c53e0e557397597dcde",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/80c122533f0ad7d3cea419889792bb8576364110"
+        },
+        "date": 1724901876987,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSONSchema_Compile_Basic",
+            "value": 240083.37398373717,
+            "unit": "ns/iter",
+            "extra": "iterations: 3075\ncpu: 235814.63414634147 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Validate_Draft4_Meta_1_No_Callback",
+            "value": 13229.059194050886,
+            "unit": "ns/iter",
+            "extra": "iterations: 53502\ncpu: 13223.767335800527 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Validate_Draft4_Required_Properties",
+            "value": 9494.76615768629,
+            "unit": "ns/iter",
+            "extra": "iterations: 72891\ncpu: 9491.254064287772 ns\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
