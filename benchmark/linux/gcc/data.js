3c675c35354e9e95f3d8f341dd3d265a252a1ba7
github-action-benchmark <github@users.noreply.github.com>
2024-08-29 03:45:31 +0000

    add Benchmark (linux/gcc) (googlecpp) benchmark result for 5f1e1a070854658dd704d41bc00a7826ecb5c0b6

--- benchmark/linux/gcc/data.js
+++ benchmark/linux/gcc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724902196314,
+  "lastUpdate": 1724903131807,
   "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
   "entries": {
     "Benchmark (linux/gcc)": [
@@ -84,6 +84,48 @@ window.BENCHMARK_DATA = {
             "value": 394682.1432731509,
             "unit": "ns/iter",
             "extra": "iterations: 1717\ncpu: 394674.09202096675 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "5f1e1a070854658dd704d41bc00a7826ecb5c0b6",
+          "message": "Add various pointer-related evaluator optimization TODOs (#1054)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-08-28T23:39:09-04:00",
+          "tree_id": "e7e3feb3eecf4608adb385da5be986705dd0c065",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/5f1e1a070854658dd704d41bc00a7826ecb5c0b6"
+        },
+        "date": 1724903131429,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSONSchema_Validate_Draft4_Meta_1_No_Callback",
+            "value": 13905.290039951942,
+            "unit": "ns/iter",
+            "extra": "iterations: 50562\ncpu: 13904.73100352043 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Validate_Draft4_Required_Properties",
+            "value": 9373.056541969752,
+            "unit": "ns/iter",
+            "extra": "iterations: 74794\ncpu: 9372.872503141967 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Compile_Basic",
+            "value": 393423.6467236984,
+            "unit": "ns/iter",
+            "extra": "iterations: 1755\ncpu: 393421.71737891744 ns\nthreads: 1"
           }
         ]
       }
