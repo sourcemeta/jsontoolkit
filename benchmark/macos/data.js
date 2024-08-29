2534862dd7a3010e2becfde3f41f947a4d4eb8b7
github-action-benchmark <github@users.noreply.github.com>
2024-08-29 03:25:29 +0000

    add Benchmark (macos) (googlecpp) benchmark result for 79253e81f99a3ea39f4cda62fa3be2a0cda0d12c

--- benchmark/macos/data.js
+++ benchmark/macos/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1724901878797,
+  "lastUpdate": 1724901929681,
   "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
   "entries": {
     "Benchmark (macos)": [
@@ -42,6 +42,48 @@ window.BENCHMARK_DATA = {
             "value": 9494.76615768629,
             "unit": "ns/iter",
             "extra": "iterations: 72891\ncpu: 9491.254064287772 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "79253e81f99a3ea39f4cda62fa3be2a0cda0d12c",
+          "message": "Optimize how we determine instance location on target resolution (#1056)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-08-28T23:19:59-04:00",
+          "tree_id": "21ffbaaf0bbb1ba5b227ee04c646f483adcd3ed8",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/79253e81f99a3ea39f4cda62fa3be2a0cda0d12c"
+        },
+        "date": 1724901928040,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSONSchema_Compile_Basic",
+            "value": 199348.8630137186,
+            "unit": "ns/iter",
+            "extra": "iterations: 3504\ncpu: 199335.90182648404 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Validate_Draft4_Meta_1_No_Callback",
+            "value": 9553.853360572954,
+            "unit": "ns/iter",
+            "extra": "iterations: 73916\ncpu: 9552.749066507933 ns\nthreads: 1"
+          },
+          {
+            "name": "JSONSchema_Validate_Draft4_Required_Properties",
+            "value": 8125.081818804495,
+            "unit": "ns/iter",
+            "extra": "iterations: 87596\ncpu: 8123.727110826985 ns\nthreads: 1"
           }
         ]
       }
