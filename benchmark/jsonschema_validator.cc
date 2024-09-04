8b576603db958c7371e62856da44a2952eb69338
Juan Cruz Viotti <jv@jviotti.com>
2024-09-04 10:41:47 -0400

    More benchmarks for `properties`/`patternProperties`/`additionalProperties`
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/jsonschema_validator.cc
+++ benchmark/jsonschema_validator.cc
@@ -380,8 +380,141 @@ static void JSONSchema_Validate_Draft4_Nested_Object(benchmark::State &state) {
   }
 }
 
+static void
+JSONSchema_Validate_Draft4_Properties_Triad_Optional(benchmark::State &state) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "properties": {
+      "a": { "type": "boolean" },
+      "b": { "type": "boolean" },
+      "c": { "type": "boolean" },
+      "d": { "type": "boolean" },
+      "e": { "type": "boolean" },
+      "f": { "type": "boolean" }
+    },
+    "patternProperties": {
+      "^[A-Z]": { "type": "null" }
+    },
+    "additionalProperties": {
+      "type": "string"
+    }
+  })JSON")};
+
+  const sourcemeta::jsontoolkit::JSON instance{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "A": null,
+    "Z": null,
+    "foo": "bar",
+    "a": true,
+    "c": true,
+    "d": true,
+    "e": true
+  })JSON")};
+
+  const auto schema_template{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  for (auto _ : state) {
+    auto result{sourcemeta::jsontoolkit::evaluate(schema_template, instance)};
+    assert(result);
+    benchmark::DoNotOptimize(result);
+  }
+}
+
+static void
+JSONSchema_Validate_Draft4_Properties_Triad_Required(benchmark::State &state) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "required": [ "a", "A", "d" ],
+    "properties": {
+      "a": { "type": "boolean" },
+      "b": { "type": "boolean" },
+      "c": { "type": "boolean" },
+      "d": { "type": "boolean" },
+      "e": { "type": "boolean" },
+      "f": { "type": "boolean" }
+    },
+    "patternProperties": {
+      "^[A-Z]": { "type": "null" }
+    },
+    "additionalProperties": {
+      "type": "string"
+    }
+  })JSON")};
+
+  const sourcemeta::jsontoolkit::JSON instance{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "A": null,
+    "Z": null,
+    "foo": "bar",
+    "a": true,
+    "c": true,
+    "d": true,
+    "e": true
+  })JSON")};
+
+  const auto schema_template{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  for (auto _ : state) {
+    auto result{sourcemeta::jsontoolkit::evaluate(schema_template, instance)};
+    assert(result);
+    benchmark::DoNotOptimize(result);
+  }
+}
+
+static void
+JSONSchema_Validate_Draft4_Properties_Triad_Closed(benchmark::State &state) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "properties": {
+      "a": { "type": "boolean" },
+      "b": { "type": "boolean" },
+      "c": { "type": "boolean" },
+      "d": { "type": "boolean" },
+      "e": { "type": "boolean" },
+      "f": { "type": "boolean" }
+    },
+    "patternProperties": {
+      "^[A-Z]": { "type": "null" }
+    },
+    "additionalProperties": false
+  })JSON")};
+
+  const sourcemeta::jsontoolkit::JSON instance{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "A": null,
+    "Z": null,
+    "a": true,
+    "c": true,
+    "d": true,
+    "e": true
+  })JSON")};
+
+  const auto schema_template{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+
+  for (auto _ : state) {
+    auto result{sourcemeta::jsontoolkit::evaluate(schema_template, instance)};
+    assert(result);
+    benchmark::DoNotOptimize(result);
+  }
+}
+
 BENCHMARK(JSONSchema_Validate_Draft4_Meta_1_No_Callback);
 BENCHMARK(JSONSchema_Validate_Draft4_Required_Properties);
 BENCHMARK(JSONSchema_Validate_Draft4_Optional_Properties_Minimal_Match);
 BENCHMARK(JSONSchema_Validate_Draft4_Items_Schema);
 BENCHMARK(JSONSchema_Validate_Draft4_Nested_Object);
+BENCHMARK(JSONSchema_Validate_Draft4_Properties_Triad_Optional);
+BENCHMARK(JSONSchema_Validate_Draft4_Properties_Triad_Closed);
+BENCHMARK(JSONSchema_Validate_Draft4_Properties_Triad_Required);
