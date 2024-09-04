72f4ce2169a8d9704d82a8ff0833ffcac616649e
Juan Cruz Viotti <jv@jviotti.com>
2024-09-03 21:00:10 -0400

    Support JSONL input in the benchmark validate helper (#1126)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/validator_helper.cc
+++ benchmark/validator_helper.cc
@@ -1,4 +1,5 @@
 #include <sourcemeta/jsontoolkit/json.h>
+#include <sourcemeta/jsontoolkit/jsonl.h>
 #include <sourcemeta/jsontoolkit/jsonschema.h>
 
 // This is just a convenience script to run validation under a profiler,
@@ -8,6 +9,7 @@
 #include <cstdlib>    // EXIT_SUCCESS, EXIT_FAILURE
 #include <filesystem> // std::filesystem::path
 #include <iostream>   // std::cerr
+#include <vector>     // std::vector
 
 auto main(int argc, char **argv) noexcept -> int {
   if (argc < 3) {
@@ -15,20 +17,40 @@ auto main(int argc, char **argv) noexcept -> int {
     return EXIT_FAILURE;
   }
 
+  // Get the schema
   const auto schema{sourcemeta::jsontoolkit::from_file(argv[1])};
-  const auto instance{sourcemeta::jsontoolkit::from_file(argv[2])};
-
   const auto schema_template{sourcemeta::jsontoolkit::compile(
       schema, sourcemeta::jsontoolkit::default_schema_walker,
       sourcemeta::jsontoolkit::official_resolver,
-      sourcemeta::jsontoolkit::default_schema_compiler)};
+      sourcemeta::jsontoolkit::default_schema_compiler,
+      sourcemeta::jsontoolkit::SchemaCompilerCompilationMode::Optimized)};
 
+  // Get the instance/s
+  const std::filesystem::path instance_path{argv[2]};
+  std::vector<sourcemeta::jsontoolkit::JSON> instances;
+  if (instance_path.extension() == ".jsonl") {
+    std::cerr << "Interpreting instance as JSONL\n";
+    auto stream{sourcemeta::jsontoolkit::read_file(instance_path)};
+    for (const auto &instance : sourcemeta::jsontoolkit::JSONL{stream}) {
+      instances.push_back(instance);
+    }
+  } else {
+    instances.push_back(sourcemeta::jsontoolkit::from_file(instance_path));
+  }
+  std::cerr << "Number of instances: " << instances.size() << "\n";
+
+  // Validate and measure
   const auto timestamp_start{std::chrono::high_resolution_clock::now()};
-  const auto result{
-      sourcemeta::jsontoolkit::evaluate(schema_template, instance)};
+  for (const auto &instance : instances) {
+    const auto result{
+        sourcemeta::jsontoolkit::evaluate(schema_template, instance)};
+    if (!result) {
+      return EXIT_FAILURE;
+    }
+  }
   const auto timestamp_end{std::chrono::high_resolution_clock::now()};
   const auto duration{std::chrono::duration_cast<std::chrono::nanoseconds>(
       timestamp_end - timestamp_start)};
   std::cout << duration.count() << "ns\n";
-  return result ? EXIT_SUCCESS : EXIT_FAILURE;
+  return EXIT_SUCCESS;
 }
