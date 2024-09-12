d104195e42e3aa30c35714785e4219b5acc6ad02
Tony Gorez <gorez.tony@gmail.com>
2024-09-12 10:32:26 +0200

    test(jsonpointer): cover to_string for WeakPointer
    Signed-off-by: Tony Gorez <gorez.tony@gmail.com>

--- test/jsonpointer/jsonpointer_to_string_test.cc
+++ test/jsonpointer/jsonpointer_to_string_test.cc
@@ -20,3 +20,29 @@ TEST(JSONPointer_to_string, foo_1_2) {
   const sourcemeta::jsontoolkit::Pointer pointer{"foo", 1, 2};
   EXPECT_EQ(sourcemeta::jsontoolkit::to_string(pointer), "/foo/1/2");
 }
+
+TEST(JSONWeakPointer_to_string, empty) {
+  const sourcemeta::jsontoolkit::WeakPointer pointer;
+  EXPECT_EQ(sourcemeta::jsontoolkit::to_string(pointer), "");
+}
+
+TEST(JSONWeakPointer_to_string, empty_property) {
+  const std::string empty = "";
+  const sourcemeta::jsontoolkit::WeakPointer pointer{std::cref(empty)};
+  EXPECT_EQ(sourcemeta::jsontoolkit::to_string(pointer), "/");
+}
+
+TEST(JSONWeakPointer_to_string, foo_bar_baz) {
+  const std::string foo = "foo";
+  const std::string bar = "bar";
+  const std::string baz = "baz";
+  const sourcemeta::jsontoolkit::WeakPointer pointer{
+      std::cref(foo), std::cref(bar), std::cref(baz)};
+  EXPECT_EQ(sourcemeta::jsontoolkit::to_string(pointer), "/foo/bar/baz");
+}
+
+TEST(JSONWeakPointer_to_string, foo_1_2) {
+  const std::string foo = "foo";
+  const sourcemeta::jsontoolkit::WeakPointer pointer{std::cref(foo), 1, 2};
+  EXPECT_EQ(sourcemeta::jsontoolkit::to_string(pointer), "/foo/1/2");
+}
