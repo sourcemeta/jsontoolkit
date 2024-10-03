b2d26648736a03f5066df0e8325bce36c941ccf3
Juan Cruz Viotti <jv@jviotti.com>
2024-10-03 10:35:31 -0400

    Benchmark `propertyNames` on Draft 6 (#1251)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- benchmark/jsonschema_validator.cc
+++ benchmark/jsonschema_validator.cc
@@ -803,6 +803,56 @@ static void JSONSchema_Validate_Draft4_Nested_Oneof(benchmark::State &state) {
   }
 }
 
+static void JSONSchema_Validate_Draft6_Property_Names(benchmark::State &state) {
+  const sourcemeta::jsontoolkit::JSON schema{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+        "$schema": "http://json-schema.org/draft-06/schema#",
+        "propertyNames": {
+          "minLength": 2
+        }
+      })JSON")};
+
+  const sourcemeta::jsontoolkit::JSON instance{
+      sourcemeta::jsontoolkit::parse(R"JSON({
+        "aa": true,
+        "bb": true,
+        "cc": true,
+        "dd": true,
+        "ee": true,
+        "ff": true,
+        "gg": true,
+        "hh": true,
+        "ii": true,
+        "jj": true,
+        "kk": true,
+        "ll": true,
+        "mm": true,
+        "nn": true,
+        "oo": true,
+        "pp": true,
+        "qq": true,
+        "rr": true,
+        "ss": true,
+        "tt": true,
+        "uu": true,
+        "vv": true,
+        "ww": true,
+        "xx": true,
+        "yy": true,
+        "zz": true
+      })JSON")};
+
+  const auto schema_template{sourcemeta::jsontoolkit::compile(
+      schema, sourcemeta::jsontoolkit::default_schema_walker,
+      sourcemeta::jsontoolkit::official_resolver,
+      sourcemeta::jsontoolkit::default_schema_compiler)};
+  for (auto _ : state) {
+    auto result{sourcemeta::jsontoolkit::evaluate(schema_template, instance)};
+    assert(result);
+    benchmark::DoNotOptimize(result);
+  }
+}
+
 BENCHMARK(JSONSchema_Validate_Draft4_Meta_1_No_Callback);
 BENCHMARK(JSONSchema_Validate_Draft4_Required_Properties);
 BENCHMARK(JSONSchema_Validate_Draft4_Many_Optional_Properties_Minimal_Match);
@@ -817,3 +867,4 @@ BENCHMARK(JSONSchema_Validate_Draft4_Pattern_Properties_True);
 BENCHMARK(JSONSchema_Validate_Draft4_Ref_To_Single_Property);
 BENCHMARK(JSONSchema_Validate_Draft4_Additional_Properties_Type);
 BENCHMARK(JSONSchema_Validate_Draft4_Nested_Oneof);
+BENCHMARK(JSONSchema_Validate_Draft6_Property_Names);
