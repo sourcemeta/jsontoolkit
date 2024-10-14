e161b34fc97733f486c213394c3ac705ba8d7969
Juan Cruz Viotti <jv@jviotti.com>
2024-10-14 18:00:34 -0400

    Don't use C++ ranges just yet (Emscripten support is limited)
    This reverts commit 5ceffcc7c5149d65fbd07ad93e2f3ffa32a98023.
    
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsonschema/walker.cc
+++ src/jsonschema/walker.cc
@@ -2,10 +2,9 @@
 #include <sourcemeta/jsontoolkit/jsonschema.h>
 #include <sourcemeta/jsontoolkit/jsonschema_walker.h>
 
-#include <algorithm> // std::max
+#include <algorithm> // std::max, std::sort
 #include <cassert>   // assert
 #include <numeric>   // std::accumulate
-#include <ranges>    // std::ranges::sort
 
 auto sourcemeta::jsontoolkit::keyword_priority(
     std::string_view keyword, const std::map<std::string, bool> &vocabularies,
@@ -205,8 +204,8 @@ sourcemeta::jsontoolkit::SchemaKeywordIterator::SchemaKeywordIterator(
   }
 
   // Sort keywords based on priority for correct evaluation
-  std::ranges::sort(
-      this->entries,
+  std::sort(
+      this->entries.begin(), this->entries.end(),
       [&vocabularies, &walker](const auto &left, const auto &right) -> bool {
         // These cannot be empty or indexes, as we created
         // the entries array from a JSON object
