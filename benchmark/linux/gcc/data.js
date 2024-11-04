ed13286914a65d442f3dc2e3bc2785cd357aa6a6
github-action-benchmark <github@users.noreply.github.com>
2024-11-04 14:25:15 +0000

    add Benchmark (linux/gcc) (googlecpp) benchmark result for 6b7eac4d388977781c7ec52cab8de64887384ac8

--- benchmark/linux/gcc/data.js
+++ benchmark/linux/gcc/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1730730220000,
+  "lastUpdate": 1730730315689,
   "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
   "entries": {
     "Benchmark (linux/gcc)": [
@@ -23430,6 +23430,36 @@ window.BENCHMARK_DATA = {
             "value": 3273.007339911912,
             "unit": "ns/iter",
             "extra": "iterations: 230248\ncpu: 3272.9830400264063 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jv@jviotti.com",
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6b7eac4d388977781c7ec52cab8de64887384ac8",
+          "message": "Rename `resolve_from_if_absolute` to `try_resolve_from` (#1319)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-11-04T10:20:48-04:00",
+          "tree_id": "e921a5a9e5b65f9d92e2b694adaacb8c4d9e5afd",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/6b7eac4d388977781c7ec52cab8de64887384ac8"
+        },
+        "date": 1730730314470,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSON_Array_Of_Objects_Unique",
+            "value": 3054.9742694780916,
+            "unit": "ns/iter",
+            "extra": "iterations: 230349\ncpu: 3054.9203252456055 ns\nthreads: 1"
           }
         ]
       }
