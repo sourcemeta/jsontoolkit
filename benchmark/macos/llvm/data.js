b79325659b274146f464bec26e78af3f05b21d1a
github-action-benchmark <github@users.noreply.github.com>
2024-11-11 19:06:37 +0000

    add Benchmark (macos/llvm) (googlecpp) benchmark result for 7a398224cc2e76ea9ae8541a872b131c7c025267

--- benchmark/macos/llvm/data.js
+++ benchmark/macos/llvm/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1731092769406,
+  "lastUpdate": 1731351997182,
   "repoUrl": "https://github.com/sourcemeta/jsontoolkit",
   "entries": {
     "Benchmark (macos/llvm)": [
@@ -18342,6 +18342,34 @@ window.BENCHMARK_DATA = {
             "value": 3567.4211618468216,
             "unit": "ns/iter",
             "extra": "iterations: 197582\ncpu: 3565.7954671984303 ns\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "Juan Cruz Viotti",
+            "username": "jviotti",
+            "email": "jv@jviotti.com"
+          },
+          "committer": {
+            "name": "GitHub",
+            "username": "web-flow",
+            "email": "noreply@github.com"
+          },
+          "id": "7a398224cc2e76ea9ae8541a872b131c7c025267",
+          "message": "Throw if cannot determine base dialect in `frame()` (#1322)\n\nSigned-off-by: Juan Cruz Viotti <jv@jviotti.com>",
+          "timestamp": "2024-11-07T16:41:40Z",
+          "url": "https://github.com/sourcemeta/jsontoolkit/commit/7a398224cc2e76ea9ae8541a872b131c7c025267"
+        },
+        "date": 1731351994746,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "JSON_Array_Of_Objects_Unique",
+            "value": 5016.32897637866,
+            "unit": "ns/iter",
+            "extra": "iterations: 151941\ncpu: 4650.3247971252 ns\nthreads: 1"
           }
         ]
       }
