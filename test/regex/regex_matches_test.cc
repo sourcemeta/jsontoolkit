7e5aeb598373eb96bf69ef439bdb0e16a846e69a
Juan Cruz Viotti <jv@jviotti.com>
2024-11-22 15:59:34 -0400

    Add more regex benchmarks and tests (#1338)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/regex/regex_matches_test.cc
+++ test/regex/regex_matches_test.cc
@@ -32,6 +32,37 @@ TEST(Regex_matches, match_true_5) {
   EXPECT_TRUE(sourcemeta::jsontoolkit::matches(regex.value(), "foobar"));
 }
 
+TEST(Regex_matches, match_true_6) {
+  const auto regex{sourcemeta::jsontoolkit::to_regex("^/.*")};
+  EXPECT_TRUE(regex.has_value());
+  EXPECT_TRUE(sourcemeta::jsontoolkit::matches(regex.value(), "/foo/bar"));
+}
+
+TEST(Regex_matches, match_true_7) {
+  const auto regex{sourcemeta::jsontoolkit::to_regex("^/.*")};
+  EXPECT_TRUE(regex.has_value());
+  EXPECT_TRUE(sourcemeta::jsontoolkit::matches(regex.value(), "/"));
+}
+
+TEST(Regex_matches, match_true_8) {
+  const auto regex{sourcemeta::jsontoolkit::to_regex("^.{1,256}$")};
+  EXPECT_TRUE(regex.has_value());
+  EXPECT_TRUE(sourcemeta::jsontoolkit::matches(regex.value(), "foo"));
+}
+
+TEST(Regex_matches, match_true_9) {
+  const auto regex{sourcemeta::jsontoolkit::to_regex("^.{1,256}$")};
+  EXPECT_TRUE(regex.has_value());
+  EXPECT_TRUE(sourcemeta::jsontoolkit::matches(regex.value(), "f"));
+}
+
+TEST(Regex_matches, match_true_10) {
+  const auto regex{sourcemeta::jsontoolkit::to_regex("^.{1,256}$")};
+  EXPECT_TRUE(regex.has_value());
+  EXPECT_TRUE(
+      sourcemeta::jsontoolkit::matches(regex.value(), std::string(256, 'x')));
+}
+
 TEST(Regex_matches, match_false_1) {
   const auto regex{sourcemeta::jsontoolkit::to_regex("^foo")};
   EXPECT_TRUE(regex.has_value());
@@ -42,4 +73,17 @@ TEST(Regex_matches, match_false_2) {
   const auto regex{sourcemeta::jsontoolkit::to_regex(".+")};
   EXPECT_TRUE(regex.has_value());
   EXPECT_FALSE(sourcemeta::jsontoolkit::matches(regex.value(), ""));
+}
+
+TEST(Regex_matches, match_false_3) {
+  const auto regex{sourcemeta::jsontoolkit::to_regex("^/.*")};
+  EXPECT_TRUE(regex.has_value());
+  EXPECT_FALSE(sourcemeta::jsontoolkit::matches(regex.value(), "foo/bar"));
+}
+
+TEST(Regex_matches, match_false_4) {
+  const auto regex{sourcemeta::jsontoolkit::to_regex("^.{1,256}$")};
+  EXPECT_TRUE(regex.has_value());
+  EXPECT_FALSE(
+      sourcemeta::jsontoolkit::matches(regex.value(), std::string(257, 'x')));
 }
