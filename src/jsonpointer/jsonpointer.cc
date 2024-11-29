a404939014b212f5465f8d9949ad6bca8531e187
Juan Cruz Viotti <jv@jviotti.com>
2024-11-29 22:07:31 +0100

    Use raw pointers when traversing JSON Pointer
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/jsonpointer/jsonpointer.cc
+++ src/jsonpointer/jsonpointer.cc
@@ -13,6 +13,51 @@
 #include <utility>     // std::move
 
 namespace {
+
+template <template <typename T> typename Allocator, typename V,
+          typename PointerT =
+              sourcemeta::jsontoolkit::GenericPointer<typename V::String>>
+auto traverse_all(V &document, const PointerT &pointer) -> V & {
+  // Make sure types match
+  static_assert(
+      std::is_same_v<typename PointerT::Value, std::remove_const_t<V>>);
+
+  V *current = &document;
+
+  // Evaluation of a JSON Pointer begins with a reference to the root
+  // value of a JSON document and completes with a reference to some value
+  // within the document.  Each reference token in the JSON Pointer is
+  // evaluated sequentially.
+  // See https://www.rfc-editor.org/rfc/rfc6901#section-4
+  for (const auto &token : pointer) {
+    if (token.is_property()) {
+      // If the currently referenced value is a JSON object, the new
+      // referenced value is the object member with the name identified by
+      // the reference token.  The member name is equal to the token if it
+      // has the same number of Unicode characters as the token and their
+      // code points are byte-by-byte equal.  No Unicode character
+      // normalization is performed.
+      // See https://www.rfc-editor.org/rfc/rfc6901#section-4
+      current = &current->at(token.to_property());
+    } else {
+      // If the currently referenced value is a JSON array, the reference
+      // token MUST contain [...] characters comprised of digits (see ABNF
+      // below; note that leading zeros are not allowed) that represent an
+      // unsigned base-10 integer value, making the new referenced value the
+      // array element with the zero-based index identified by the
+      // token.
+      // See https://www.rfc-editor.org/rfc/rfc6901#section-4
+      if (current->is_object()) {
+        current = &current->at(std::to_string(token.to_index()));
+      } else {
+        current = &current->at(token.to_index());
+      }
+    }
+  }
+
+  return *current;
+}
+
 template <template <typename T> typename Allocator, typename V,
           typename PointerT =
               sourcemeta::jsontoolkit::GenericPointer<typename V::String>>
@@ -22,7 +67,7 @@ auto traverse(V &document, typename PointerT::const_iterator begin,
   static_assert(
       std::is_same_v<typename PointerT::Value, std::remove_const_t<V>>);
 
-  std::reference_wrapper<V> current{document};
+  V *current = &document;
 
   // Evaluation of a JSON Pointer begins with a reference to the root
   // value of a JSON document and completes with a reference to some value
@@ -38,7 +83,7 @@ auto traverse(V &document, typename PointerT::const_iterator begin,
       // code points are byte-by-byte equal.  No Unicode character
       // normalization is performed.
       // See https://www.rfc-editor.org/rfc/rfc6901#section-4
-      current = current.get().at(iterator->to_property());
+      current = &current->at(iterator->to_property());
     } else {
       // If the currently referenced value is a JSON array, the reference
       // token MUST contain [...] characters comprised of digits (see ABNF
@@ -47,15 +92,15 @@ auto traverse(V &document, typename PointerT::const_iterator begin,
       // array element with the zero-based index identified by the
       // token.
       // See https://www.rfc-editor.org/rfc/rfc6901#section-4
-      if (current.get().is_object()) {
-        current = current.get().at(std::to_string(iterator->to_index()));
+      if (current->is_object()) {
+        current = &current->at(std::to_string(iterator->to_index()));
       } else {
-        current = current.get().at(iterator->to_index());
+        current = &current->at(iterator->to_index());
       }
     }
   }
 
-  return current.get();
+  return *current;
 }
 
 template <typename PointerT>
@@ -110,8 +155,7 @@ auto get(const JSON &document, const Pointer &pointer) -> const JSON & {
     return document;
   }
 
-  return traverse<std::allocator, const JSON>(document, std::cbegin(pointer),
-                                              std::cend(pointer));
+  return traverse_all<std::allocator, const JSON>(document, pointer);
 }
 
 auto get(const JSON &document, const WeakPointer &pointer) -> const JSON & {
@@ -119,8 +163,8 @@ auto get(const JSON &document, const WeakPointer &pointer) -> const JSON & {
     return document;
   }
 
-  return traverse<std::allocator, const JSON, WeakPointer>(
-      document, std::cbegin(pointer), std::cend(pointer));
+  return traverse_all<std::allocator, const JSON, WeakPointer>(document,
+                                                               pointer);
 }
 
 auto get(JSON &document, const Pointer &pointer) -> JSON & {
@@ -128,8 +172,7 @@ auto get(JSON &document, const Pointer &pointer) -> JSON & {
     return document;
   }
 
-  return traverse<std::allocator, JSON>(document, std::cbegin(pointer),
-                                        std::cend(pointer));
+  return traverse_all<std::allocator, JSON>(document, pointer);
 }
 
 auto try_get(const JSON &document, const Pointer &pointer)
