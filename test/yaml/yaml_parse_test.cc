c8626b96f07498aeceeab231ad402648283305c6
Juan Cruz Viotti <jv@jviotti.com>
2025-01-14 14:34:02 -0400

    Add a single YAML object parsing test
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/yaml/yaml_parse_test.cc
+++ test/yaml/yaml_parse_test.cc
@@ -3,6 +3,19 @@
 #include <sourcemeta/jsontoolkit/json.h>
 #include <sourcemeta/jsontoolkit/yaml.h>
 
+TEST(YAML_parse, object_2) {
+  const std::string input{"foo: >\n  bar\n  baz"};
+
+  const auto result{sourcemeta::jsontoolkit::from_yaml(input)};
+
+  const sourcemeta::jsontoolkit::JSON expected =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "foo": "bar baz"
+  })JSON");
+
+  EXPECT_EQ(result, expected);
+}
+
 TEST(YAML_parse, empty) {
   const std::string input{""};
   EXPECT_THROW(sourcemeta::jsontoolkit::from_yaml(input),
