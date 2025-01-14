992e7c9b7c2ec3c6b37adcd744d47c3b6c115bab
Juan Cruz Viotti <jv@jviotti.com>
2025-01-14 14:34:02 -0400

    Add an initial basic YAML parsing test
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/yaml/yaml_parse_test.cc
+++ test/yaml/yaml_parse_test.cc
@@ -3,6 +3,17 @@
 #include <sourcemeta/jsontoolkit/json.h>
 #include <sourcemeta/jsontoolkit/yaml.h>
 
+TEST(YAML_parse, array_1) {
+  const std::string input{"- foo\n- true"};
+
+  const auto result{sourcemeta::jsontoolkit::from_yaml(input)};
+
+  const sourcemeta::jsontoolkit::JSON expected =
+      sourcemeta::jsontoolkit::parse(R"JSON([ "foo", true ])JSON");
+
+  EXPECT_EQ(result, expected);
+}
+
 TEST(YAML_parse, empty) {
   const std::string input{""};
   EXPECT_THROW(sourcemeta::jsontoolkit::from_yaml(input),
