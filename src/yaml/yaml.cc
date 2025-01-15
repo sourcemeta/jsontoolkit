9123cc55dd480f58fe2ffc171a555a91365f8c20
Juan Cruz Viotti <jv@jviotti.com>
2025-01-15 12:40:29 -0400

    Finish a simple YAML integration (#1445)
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- src/yaml/yaml.cc
+++ src/yaml/yaml.cc
@@ -1,112 +1,103 @@
 #include <sourcemeta/jsontoolkit/yaml.h>
 
-#include <memory>      // std::unique_ptr
-#include <string>      // std::string
 #include <string_view> // std::string_view
 
 // See https://pyyaml.org/wiki/LibYAML for basic documentation
 #include <yaml.h>
 
-class YAMLParser {
-public:
-  YAMLParser() {
-    if (!yaml_parser_initialize(&this->parser)) {
-      throw sourcemeta::jsontoolkit::YAMLError(
-          "Could not initialize the YAML parser");
-    }
-
-    yaml_parser_set_encoding(&this->parser, YAML_UTF8_ENCODING);
+static auto yaml_node_to_json(yaml_node_t *const node,
+                              yaml_document_t *const document)
+    -> sourcemeta::jsontoolkit::JSON {
+  if (!node) {
+    return sourcemeta::jsontoolkit::JSON{nullptr};
   }
 
-  ~YAMLParser() { yaml_parser_delete(&this->parser); }
+  switch (node->type) {
+    case YAML_SCALAR_NODE: {
+      const std::string_view input{
+          reinterpret_cast<char *>(node->data.scalar.value),
+          node->data.scalar.length};
 
-  auto parse(const sourcemeta::jsontoolkit::JSON::String &input)
-      -> const sourcemeta::jsontoolkit::JSON & {
-    yaml_parser_set_input_string(
-        &this->parser, reinterpret_cast<const unsigned char *>(input.c_str()),
-        input.size());
-    this->handle_next_event();
-    return this->result;
-  }
-
-private:
-  auto handle_next_event() -> void {
-    yaml_event_t event{};
-    if (!yaml_parser_parse(&this->parser, &event)) {
-      throw sourcemeta::jsontoolkit::YAMLError(
-          "Could parse the next YAML token");
+      try {
+        // TODO: Avoid this std::string transformation
+        return sourcemeta::jsontoolkit::parse(std::string{input});
+        // Looks like it is very hard in YAML, given a scalar value, to
+        // determine whether it is a string or something else without attempting
+        // to parsing it and potentially failing to do so
+      } catch (const sourcemeta::jsontoolkit::ParseError &) {
+        return sourcemeta::jsontoolkit::JSON{input};
+      }
     }
 
-    std::unique_ptr<yaml_event_t, void (*)(yaml_event_t *)> event_guard(
-        &event, [](yaml_event_t *data) {
-          if (data) {
-            yaml_event_delete(data);
-          }
-        });
-
-    switch (event.type) {
-      case YAML_STREAM_START_EVENT:
-        this->handle_next_event();
-        break;
-      case YAML_STREAM_END_EVENT:
-        if (!this->has_parsed) {
-          // TODO: Ideally we would get line/column information like for
-          // `ParseError`, possive with `yaml_mark_t` in `yaml_event_t`
-          throw sourcemeta::jsontoolkit::YAMLParseError(
-              "Failed to parse the YAML document");
-        }
-
-        break;
-      case YAML_DOCUMENT_START_EVENT:
-        this->handle_next_event();
-        break;
-      case YAML_DOCUMENT_END_EVENT:
-        this->handle_next_event();
-        break;
-
-      case YAML_NO_EVENT:
-        // TODO: Ideally we would get line/column information like for
-        // `ParseError`, possive with `yaml_mark_t` in `yaml_event_t`
-        throw sourcemeta::jsontoolkit::YAMLParseError(
-            "Failed to parse the YAML document");
-
-      case YAML_SCALAR_EVENT: {
-        this->has_parsed = true;
-        const std::string_view input{
-            reinterpret_cast<char *>(event.data.scalar.value),
-            event.data.scalar.length};
-
-        try {
-          // TODO: Avoid this std::string transformation
-          this->current = sourcemeta::jsontoolkit::parse(std::string{input});
-          // Looks like it is very hard in YAML, given a scalar value, to
-          // determine whether it is a string or something else without
-          // attempting to parsing it and potentially failing to do so
-        } catch (const sourcemeta::jsontoolkit::ParseError &) {
-          this->current = sourcemeta::jsontoolkit::JSON{input};
-        }
-
-        this->handle_next_event();
-        break;
+    case YAML_SEQUENCE_NODE: {
+      auto result{sourcemeta::jsontoolkit::JSON::make_array()};
+      for (yaml_node_item_t *item = node->data.sequence.items.start;
+           item < node->data.sequence.items.top; ++item) {
+        yaml_node_t *const child = yaml_document_get_node(document, *item);
+        result.push_back(yaml_node_to_json(child, document));
       }
 
-      default:
-        assert(false);
-        break;
+      return result;
     }
-  }
 
-  sourcemeta::jsontoolkit::JSON result{nullptr};
-  sourcemeta::jsontoolkit::JSON &current = result;
-  bool has_parsed{false};
-  yaml_parser_t parser;
-};
+    case YAML_MAPPING_NODE: {
+      auto result{sourcemeta::jsontoolkit::JSON::make_object()};
+      for (yaml_node_pair_t *pair = node->data.mapping.pairs.start;
+           pair < node->data.mapping.pairs.top; ++pair) {
+        yaml_node_t *const key_node =
+            yaml_document_get_node(document, pair->key);
+        yaml_node_t *const value_node =
+            yaml_document_get_node(document, pair->value);
+        if (key_node && key_node->type == YAML_SCALAR_NODE) {
+          result.assign(reinterpret_cast<char *>(key_node->data.scalar.value),
+                        yaml_node_to_json(value_node, document));
+        }
+      }
+
+      return result;
+    }
+
+    default:
+      return sourcemeta::jsontoolkit::JSON{nullptr};
+  }
+}
 
 namespace sourcemeta::jsontoolkit {
 
 auto from_yaml(const JSON::String &input) -> JSON {
-  YAMLParser yaml;
-  return yaml.parse(input);
+  yaml_parser_t parser;
+  if (!yaml_parser_initialize(&parser)) {
+    throw YAMLError("Could not initialize the YAML parser");
+  }
+
+  yaml_parser_set_input_string(
+      &parser, reinterpret_cast<const unsigned char *>(input.c_str()),
+      input.size());
+
+  yaml_document_t document;
+  if (!yaml_parser_load(&parser, &document)) {
+    yaml_parser_delete(&parser);
+    // TODO: Ideally we would get line/column information like for `ParseError`
+    throw YAMLParseError("Failed to parse the YAML document");
+  }
+
+  yaml_node_t *const root = yaml_document_get_root_node(&document);
+  if (!root) {
+    yaml_document_delete(&document);
+    yaml_parser_delete(&parser);
+    throw YAMLParseError("The input YAML document is empty");
+  }
+
+  try {
+    const auto result{yaml_node_to_json(root, &document)};
+    yaml_document_delete(&document);
+    yaml_parser_delete(&parser);
+    return result;
+  } catch (...) {
+    yaml_document_delete(&document);
+    yaml_parser_delete(&parser);
+    throw;
+  }
 }
 
 } // namespace sourcemeta::jsontoolkit
--- test/yaml/yaml_parse_test.cc
+++ test/yaml/yaml_parse_test.cc
@@ -10,6 +10,45 @@ TEST(YAML_parse, scalar_1) {
   EXPECT_EQ(result, expected);
 }
 
+TEST(YAML_parse, object_1) {
+  const std::string input{"hello: world\nfoo: 1\nbar: true"};
+
+  const auto result{sourcemeta::jsontoolkit::from_yaml(input)};
+
+  const sourcemeta::jsontoolkit::JSON expected =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "hello": "world",
+    "foo": 1,
+    "bar": true
+  })JSON");
+
+  EXPECT_EQ(result, expected);
+}
+
+TEST(YAML_parse, object_2) {
+  const std::string input{"foo: >\n  bar\n  baz"};
+
+  const auto result{sourcemeta::jsontoolkit::from_yaml(input)};
+
+  const sourcemeta::jsontoolkit::JSON expected =
+      sourcemeta::jsontoolkit::parse(R"JSON({
+    "foo": "bar baz"
+  })JSON");
+
+  EXPECT_EQ(result, expected);
+}
+
+TEST(YAML_parse, array_1) {
+  const std::string input{"- foo\n- true"};
+
+  const auto result{sourcemeta::jsontoolkit::from_yaml(input)};
+
+  const sourcemeta::jsontoolkit::JSON expected =
+      sourcemeta::jsontoolkit::parse(R"JSON([ "foo", true ])JSON");
+
+  EXPECT_EQ(result, expected);
+}
+
 TEST(YAML_parse, empty) {
   const std::string input{""};
   EXPECT_THROW(sourcemeta::jsontoolkit::from_yaml(input),
@@ -18,6 +57,12 @@ TEST(YAML_parse, empty) {
 
 TEST(YAML_parse, blank) {
   const std::string input{"    "};
+  EXPECT_THROW(sourcemeta::jsontoolkit::from_yaml(input),
+               sourcemeta::jsontoolkit::YAMLParseError);
+}
+
+TEST(YAML_parse, invalid_1) {
+  const std::string input{"{ xx"};
   EXPECT_THROW(sourcemeta::jsontoolkit::from_yaml(input),
                sourcemeta::jsontoolkit::YAMLParseError);
 }
